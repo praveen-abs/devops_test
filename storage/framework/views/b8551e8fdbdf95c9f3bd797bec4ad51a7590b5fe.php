5fbaa8a9fb0a1fe6dbb9a63ba50cac02ca2be8c7
praveenmax <com.java.praveen@gmail.com>
2022-05-04 19:42:44 +0530

    First Commit. Web app running well in localhost

---
+++ storage/framework/views/b8551e8fdbdf95c9f3bd797bec4ad51a7590b5fe.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <script>document.write(new Date().getFullYear())</script> © Velzon.
+            </div>
+            <div class="col-sm-6">
+                <div class="text-sm-end d-none d-sm-block">
+                    Design & Develop by Themesbrand
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<?php /**PATH E:\__Codings\ClientWorks\ABS\Prototyping\pms_prototype\resources\views/layouts/footer.blade.php ENDPATH**/ ?>
