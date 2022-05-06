2535ca477b6d9566c4a44d4fec2493c39bef4873
praveenmax <com.java.praveen@gmail.com>
2022-05-07 03:15:47 +0530

    Dashboard page : Added some card widgets in home screen

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,12 +57,100 @@
             </div>
             <!--end row-->
 
+            <div class="row">
+                <div class="col-xl-3 col-md-6">
+                    <!-- card -->
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="flex-grow-1 overflow-hidden">
+                                    <p
+                                        class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                                        Pending Tasks</p>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                <div>
+                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span
+                                            class="counter-value" data-target="6">0</span>
+                                    </h4>
+                                    <a href="" class="text-decoration-underline">More Details</a>
+                                </div>
 
+                            </div>
+                        </div><!-- end card body -->
+                    </div><!-- end card -->
+                </div><!-- end col -->
+
+                <div class="col-xl-3 col-md-6">
+                    <!-- card -->
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="flex-grow-1 overflow-hidden">
+                                    <p
+                                        class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                                        New Orders</p>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                <div>
+                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span
+                                            class="counter-value" data-target="20">0</span></h4>
+                                    <a href="" class="text-decoration-underline">View all orders</a>
+                                </div>
+                                <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-soft-info rounded fs-3">
+                                        <i class="bx bx-shopping-bag text-info"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div><!-- end card -->
+                </div><!-- end col -->
+
+                <div class="col-xl-3 col-md-6">
+                    <!-- card -->
+                    <div class="card card-animate">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="flex-grow-1 overflow-hidden">
+                                    <p
+                                        class="text-uppercase fw-medium text-muted text-truncate mb-0">
+                                        New Customers</p>
+                                </div>
+                            </div>
+                            <div class="d-flex align-items-end justify-content-between mt-4">
+                                <div>
+                                    <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span
+                                            class="counter-value" data-target="15">0</span>
+                                    </h4>
+                                    <a href="" class="text-decoration-underline">See details</a>
+                                </div>
+                                <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-soft-warning rounded fs-3">
+                                        <i class="bx bx-user-circle text-warning"></i>
+                                    </span>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div><!-- end card -->
+                </div><!-- end col -->
+
+
+
+
+
+    </div> <!-- end col -->
+
+    </div> <!-- end .rightbar-->
  
-
+ 
+    </div> <!-- end col -->
 </div>
-
+ 
 @endsection
+
 @section('script')
 <!-- apexcharts -->
 <script src="{{ URL::asset('/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
