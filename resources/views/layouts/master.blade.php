b3f1a98ad6201be30c5e738c106d33c3fbe34c28
praveenmax <com.java.praveen@gmail.com>
2022-06-03 23:53:01 +0530

    Updated webbrowser tab title.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>PMS Portal</title>
+    <title>ABS - HRMS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- App favicon -->
