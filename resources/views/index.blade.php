bbd8903fa34ce69be8daa38dd52bee92dc6b66d8
praveenmax <com.java.praveen@gmail.com>
2022-06-08 21:48:10 +0530

    Some corrections in Main dashboard

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -158,14 +158,19 @@
 
 
 
-                @endsection
+    </div>        
+</div>
+<!--end row-->
 
-                @section('script')
-                <!-- apexcharts -->
-                <script src="{{ URL::asset('/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
-                <script src="{{ URL::asset('/assets/libs/jsvectormap/jsvectormap.min.js') }}"></script>
-                <script src="{{ URL::asset('assets/libs/swiper/swiper.min.js')}}"></script>
-                <!-- dashboard init -->
-                <script src="{{ URL::asset('/assets/js/pages/dashboard-ecommerce.init.js') }}"></script>
-                <script src="{{ URL::asset('/assets/js/app.min.js') }}"></script>
-                @endsection
+
+@endsection
+
+@section('script')
+<!-- apexcharts -->
+<script src="{{ URL::asset('/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ URL::asset('/assets/libs/jsvectormap/jsvectormap.min.js') }}"></script>
+<script src="{{ URL::asset('assets/libs/swiper/swiper.min.js')}}"></script>
+<!-- dashboard init -->
+<script src="{{ URL::asset('/assets/js/pages/dashboard-ecommerce.init.js') }}"></script>
+<script src="{{ URL::asset('/assets/js/app.min.js') }}"></script>
+@endsection
