42d131b93235ef2665a831638929727e053e9fd9
george <nijenthen@gmail.com>
2022-06-10 14:37:12 +0530

    card animate added.
    live time,date,months,year added  in cards.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,230 @@
 @slot('li_1') Dashboards @endslot
 @slot('title') Dashboard @endslot
 @endcomponent
+
+
+<section class="widget-wrapper">
+
+    <div class="widget-container  container-fluid">
+
+        <div class="row">
+            <div class="col-6">
+                <!-- for add -->
+                <div class="widget-item">
+
+                    <div class="card card-content card-animate">
+                        <div class="card-body p-2 card-body-content d-flex flex-column">
+                            <div class="d-flex align-items-center justify-content-between">
+                                <p class="text-large text-white">Holidays</p><a class="text-white">View All</a>
+
+                            </div>
+                            <div class="d-flex align-items-center my-auto">
+                                <div class="icon-click"><span class=" ri-arrow-drop-left-line text-white"></span>
+
+                                </div>
+                                <div class="flex-grow-1 mx-10 text-white">
+                                    <h1 class="text-truncate-1 text-white" title="Rakshabandhan">Rakshabandhan</h1>
+                                    <p class="d-flex align-items-center mt-1"><span>Thu, 11 August, 2022</span>
+
+                                    </p>
+                                </div>
+                                <div class="icon-click">
+                                    <!---->
+                                </div>
+                            </div>
+                            <!---->
+                            <!---->
+                        </div>
+                        <div class="action-buttons">
+                            <div tooltip="Settings" class="button"><span class="icon ic-settings"></span></div>
+                            <!---->
+                            <div tooltip="Drag" class="cursor-grab"><span class="icon ic-move transform-90"></span>
+                            </div>
+                            <!---->
+                            <div tooltip="Remove" class="button"><span class="icon ic-close"></span></div>
+                            <!---->
+                        </div>
+                    </div>
+                    <!---->
+
+                    <!---->
+                </div>
+            </div>
+            <!-- for date and time -->
+            <div class="col-6">
+                <div class="widget-item ">
+
+                    <div class="card card-content card-animate">
+                        <div class="card-body p-2 card-body-content d-flex flex-column">
+                            <div class="d-flex align-items-center justify-content-between">
+
+                                <div class="date-time d-flex align-items-center justify-content-between ">
+                                    <p class="text-large text-white">
+                                        Today Date
+                                        <span>-</span>
+                                    </p>
+                                    <p id="displayDay"></p>
+                                </div>
+
+                                <p class="text-white">View All</p>
+
+                            </div>
+
+
+                            <div class="d-flex">
+                                <div class="d-flex align-items-center my-auto">
+                                    <div class="show-time d-flex flex-column">
+                                        <p>Current Time</p>
+                                        <div class="displayTime  d-flex">
+                                            <h4 id="time" class="text-white"> </h4><s id="seconds" class="px-1"></s>
+                                            <p id="session" class="px-1"></p>
+                                        </div>
+                                    </div>
+                                    <div class="flex-grow-1 mx-10 text-white">
+
+                                    </div>
+                                    <div class="icon-click">
+                                        <!---->
+                                    </div>
+                                </div>
+                            </div>
+                            <!---->
+                            <!---->
+                        </div>
+                        <div class="action-buttons">
+                            <div tooltip="Settings" class="button"><span class="icon ic-settings"></span></div>
+                            <!---->
+                            <div tooltip="Drag" class="cursor-grab"><span class="icon ic-move transform-90"></span>
+                            </div>
+                            <!---->
+                            <div tooltip="Remove" class="button"><span class="icon ic-close"></span></div>
+                            <!---->
+                        </div>
+                    </div>
+                    <!---->
+
+                    <!---->
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-6">
+                <!-- for employee info -->
+                <div class="widget-item">
+                    <div class="card card-content card-animate">
+                        <div class="card-body p-2 card-body-content d-flex flex-column">
+                            <div>
+                                <h6 class="text-white">On Leave Today</h6>
+                            </div>
+                            <div class="d-flex mt-3">
+                                <div class="">
+                                    <h5 class="text-white">Everyone Working Today</h5>
+                                    <span class="text-white">No one is on leave today</span>
+                                </div>
+                                <div>
+                                    <div class="info-img">
+
+                                    </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+
+                <div class="widget-item">
+
+                    <div class="card card-content card-animate">
+                        <div class="card-body p-2 card-body-content d-flex flex-column">
+                            <div class="d-flex align-items-center justify-content-between">
+                                <p>Quick Links</p>
+
+
+                            </div>
+
+
+                            <div class="d-flex mt-4">
+                                <div class="d-flex align-items-center my-auto">
+                                    <h4 class="text-white">No Quick Links Are Added</h4>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <!---->
+
+                    <!---->
+                </div>
+            </div>
+        </div>
+        <!-- for circle  -->
+        <div class="row">
+            <div class="col-6">
+                <div class="widget-item">
+
+                    <div class="card card-content card-animate ">
+                        <div class="card-body p-2 card-body-content  d-flex flex-column">
+
+                            <div>
+                                <span>Leave Balances</span>
+                            </div>
+
+                            <div class="d-flex mt-2">
+                                <div class="d-flex flex-column align-items-center text-center ">
+                                    <div
+                                        class=" align-items-center d-flex justify-content-around rounded-circle roundedCircle">
+                                        0</div>
+                                    <p>Sick Leave</p>
+                                </div>
+                                <div class="d-flex flex-column align-items-center  text-center  ">
+                                    <div
+                                        class="animate-circle align-items-center d-flex justify-content-around   rounded-circle roundedCircle">
+                                        0</div>
+                                    <p>Privilege Leave</p>
+                                </div>
+                                <div class="d-flex flex-column align-items-center  text-center ">
+                                    <div
+                                        class="animate-circle align-items-center d-flex justify-content-around   rounded-circle roundedCircle">
+                                        0</div>
+                                    <p>Casual Leave</p>
+                                </div>
+
+                                <div>
+                                    <p class="text-center">Apply Leave View All Balances</p>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- rigth content -->
+
+
+
+
+
+</section>
+
+
+
+
 <div class="row">
     <div class="col">
-
         <div class="h-100">
             <div class="row mb-3 pb-1">
                 <div class="col-12">
@@ -36,7 +257,7 @@
             <div class="row">
                 <div class="col-xl-4 col-md-6 ">
                     <!-- card -->
-                    <div class="card">
+                    <div class="card card-animate">
                         <div class="card-body">
                             <div class="d-flex align-items-center">
                                 <div class="avatar-sm flex-shrink-0">
@@ -62,7 +283,7 @@
 
                 <div class="col-xl-4 col-md-6 ">
                     <!-- card -->
-                    <div class="card">
+                    <div class="card card-animate">
                         <div class="card-body">
                             <div class="d-flex align-items-center">
                                 <div class="avatar-sm flex-shrink-0">
@@ -88,7 +309,7 @@
 
                 <div class="col-xl-4 col-md-6 ">
                     <!-- card -->
-                    <div class="card">
+                    <div class="card card-animate">
                         <div class="card-body">
                             <div class="d-flex align-items-center">
                                 <div class="avatar-sm flex-shrink-0">
@@ -108,9 +329,9 @@
                     </div>
                 </div><!-- end col -->
 
-                <div class="col-xl-4 col-md-6 ">
+                <div class="col-xl-4 col-md-6">
                     <!-- card -->
-                    <div class="card">
+                    <div class="card  card-animate">
                         <div class="card-body">
                             <div class="d-flex align-items-center">
                                 <div class="avatar-sm flex-shrink-0">
@@ -133,7 +354,7 @@
 
                 <div class="col-xl-4 col-md-6 ">
                     <!-- card -->
-                    <div class="card">
+                    <div class="card card-animate">
                         <div class="card-body">
                             <div class="d-flex align-items-center">
                                 <div class="avatar-sm flex-shrink-0">
@@ -158,19 +379,103 @@
 
 
 
-    </div>        
-</div>
-<!--end row-->
+        </div>
+    </div>
+    <!--end row-->
 
 
-@endsection
+    @endsection
 
-@section('script')
-<!-- apexcharts -->
-<script src="{{ URL::asset('/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ URL::asset('/assets/libs/jsvectormap/jsvectormap.min.js') }}"></script>
-<script src="{{ URL::asset('assets/libs/swiper/swiper.min.js')}}"></script>
-<!-- dashboard init -->
-<script src="{{ URL::asset('/assets/js/pages/dashboard-ecommerce.init.js') }}"></script>
-<script src="{{ URL::asset('/assets/js/app.min.js') }}"></script>
-@endsection
+    @section('script')
+    <!-- apexcharts -->
+    <script src="{{ URL::asset('/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ URL::asset('/assets/libs/jsvectormap/jsvectormap.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/libs/swiper/swiper.min.js')}}"></script>
+    <!-- dashboard init -->
+    <script src="{{ URL::asset('/assets/js/pages/dashboard-ecommerce.init.js') }}"></script>
+    <script src="{{ URL::asset('/assets/js/app.min.js') }}"></script>
+
+    <!-- for date and time -->
+
+    <script>
+    // var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    // var d = new Date(dateString);
+    // var dayName = days[d.getDay()];
+    var dayIndex = new Date().getDay();
+    const getDayName = (dayIndex) => {
+        const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        return days[dayIndex];
+    }
+    const dayName = getDayName(dayIndex)
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+
+    const d = new Date();
+
+    document.getElementById("displayDay").innerHTML = monthNames[d.getMonth()] + " " + d.getDate() + "," + d
+        .getFullYear() + " " + dayName;
+
+
+    function displayTimes() {
+
+        var date = new Date();
+        var hrs = date.getHours();
+        var mins = date.getMinutes();
+        var secs = date.getSeconds();
+
+        var sessions = document.getElementById("session");
+
+        if (hrs > 12) {
+            session.innerHTML = "PM";
+
+        } else {
+            session.innerHTML = "PM";
+        }
+
+        if (hrs > 12) {
+            hrs = hrs - 12;
+        }
+
+        if (mins <= 9) {
+            mins = "0" + mins;
+        }
+        if (secs <= 9) {
+            secs = "0" + secs;
+        }
+
+        document.getElementById("time").innerHTML = hrs + ":" + mins + ":" + secs;
+    }
+    setInterval(displayTimes, 10);
+
+
+    // for counter number
+
+    const counters = document.querySelectorAll('.count');
+    // Main function
+    for (let n of counters) {
+        const updateCount = () => {
+            const target = +n.getAttribute('data-target');
+            const count = +n.innerText;
+            const speed = 1000; // change animation speed here
+            const inc = target / speed;
+            if (count < target) {
+                n.innerText = Math.ceil(count + inc);
+                setTimeout(updateCount, 1);
+            } else {
+                n.innerText = target;
+            }
+        }
+        updateCount();
+    }
+
+
+
+    // circle prograss bar
+    </script>
+
+
+
+
+
+    @endsection
