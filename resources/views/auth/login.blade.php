e6e3c1cd5fae9e2f0d676adb25a11652d873e2bb
george <nijenthen@gmail.com>
2022-06-20 19:18:26 +0530

    removed unnecessary code in index.blade.
    added new buttons (google,facebook,linked-in) in login.blade.
    added styles in min.css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,9 +9,10 @@
         <div class="bg-overlay"></div>
 
         <!-- <div class="shape"> -->
-            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
-                 x="0px" y="0px" width="100%" height="100%" preserveAspectRatio="none">
-                <path class="path" width="100%" height="100%" fill="none" />            </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            width="100%" height="100%" preserveAspectRatio="none">
+            <path class="path" width="100%" height="100%" fill="none" />
+        </svg>
         <!-- </div> -->
         <!-- <div style=" position:absolute;bottom:-200px;width:100%;height:50px;">
             <img src="{{ URL::asset('assets/images/whitetexture.jpg')}}" alt="" height="1000">
@@ -23,7 +24,7 @@
 
 
 
-            <div class="card m-4 p-4">
+            <div class="card m-2 p-2">
                 <div class="card-body ">
                     <div class="row d-flex justify-content-center  align-items-center h-100">
 
@@ -31,6 +32,7 @@
                             <div class="right-content-logo w-100 h-100">
                                 <img src="{{ URL::asset($generalInfo->background_img)}}" alt="brand-logo"
                                     class="h-100 w-100">
+                                <img src="" alt="">
                             </div>
                         </div>
 
@@ -39,7 +41,8 @@
                         <div class="col-md-8 col-lg-6 col-xl-4">
 
                             <div class="text-center mt-2">
-                                    <img src="{{ URL::asset('assets/images/logo-sm.png') }}" alt="brand-logo" class="h-50 w-50">
+                                <img src="{{ URL::asset('assets/images/logo-sm.png') }}" alt="brand-logo"
+                                    class="h-50 w-50">
                             </div>
                             <div class="p-2 mt-4">
                                 <form action="{{ route('login') }}" method="POST">
@@ -47,8 +50,8 @@
                                     <div class="mb-3">
                                         <label for="username" class="form-label">Username</label>
                                         <input type="text" class="form-control @error('email') is-invalid @enderror"
-                                            value="{{ old('email', 'admin@gmail.com') }}" id="username"
-                                            name="email" placeholder="Enter username">
+                                            value="{{ old('email', 'admin@gmail.com') }}" id="username" name="email"
+                                            placeholder="Enter username">
                                         @error('email')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -87,13 +90,43 @@
                                     </div>
 
                                     <div class="mt-4">
-                                        <button class="btn btn-primary waves-effect waves-light w-100" type="submit">Sign-In</button>
-                                    
+                                        <button class="btn sign-in-btn text-white  waves-effect waves-light w-100"
+                                            type="submit">Sign-In</button>
+
                                     </div>
                                     <!-- <div class="mt-4 text-center">
                                         <p class="mb-0">Don't have an account ?</p>
                                     </div> -->
+                                    <div class="mb-3">
 
+                                        <div class="divider d-flex align-items-center my-3 mb-2">
+                                            <p class="text-center  mx-3 mb-0 text-muted">OR</p>
+                                        </div>
+
+                                    </div>
+                                    <div class="mb-3">
+
+                                        <div class="d-flex align-items-center justify-content-center flex-column">
+                                            <div>
+                                                <p class="text-center text-muted">Connect with</p>
+                                            </div>
+                                            <div class="d-flex align-items-center my-0">
+                                                <button class="btn  mx-1 google-btn text-white">
+                                                    <span class="ri-google-fill"></span>
+                                                    Google
+                                                </button>
+                                                <button class="btn  mx-1 linked-in-btn text-white">
+                                                    <span class="ri-linkedin-fill "></span>
+                                                    Linked-in
+                                                </button>
+                                                <button class="btn btn primary mx-1 facebook-btn text-white">
+                                                    <span class="ri-facebook-fill "></span>
+                                                    Facebook
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                    </div>
                                 </form>
                             </div>
                         </div>
