5124ed84d8b3be9df9c3eefd37cc87fbe72b293e
praveenmax <com.java.praveen@gmail.com>
2022-06-29 01:58:47 +0530

    Register page layout fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,22 +20,10 @@
         <!-- auth page content -->
         <div class="auth-page-content">
             <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="text-center mt-sm-5 mb-4 text-white-50">
-                            <div>
-                                <a href="index" class="d-inline-block auth-logo">
-                                    <img src="{{ URL::asset('assets/images/logo-light.png') }}" alt="" height="60">
-                                </a>
-                            </div>
-                            <!-- <p class="mt-3 fs-15 fw-medium">Premium Admin & Dashboard Template</p> -->
-                        </div>
-                    </div>
-                </div>
-                <!-- end row -->
+               <!-- end row -->
 
                 <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-5">
+                    <div class="">
                         <div class="card mt-4">
 
                             <div class="card-body p-4">
