83ea08c05888cbd1fa355fe74281acec4cc587a5
praveen <voidmaxtech@gmail.com>
2022-07-04 04:51:01 +0530

    Added default data to VmtLeaves table.

--- app/Models/VmtLeaves.php
+++ app/Models/VmtLeaves.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class VmtLeaves extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'leave_type',
+        'leave_max_limit',
+        'created_at',
+        'updated_at',
+    ];
 }
--- database/migrations/2022_07_03_224136_create_vmt_leaves_table.php
+++ database/migrations/2022_07_03_224136_create_vmt_leaves_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\VmtLeaves;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,6 +20,14 @@ return new class extends Migration
             $table->string("leave_max_limit")->nullable();
             $table->timestamps();
         });
+
+        VmtLeaves::create(['leave_type' => 'sick_leave','leave_max_limit' => '10']);
+        VmtLeaves::create(['leave_type' => 'lop_leave','leave_max_limit' => '5']);
+        VmtLeaves::create(['leave_type' => 'casual_leave','leave_max_limit' => '3']);
+        VmtLeaves::create(['leave_type' => 'compensatory_leave','leave_max_limit' => '2']);
+        VmtLeaves::create(['leave_type' => 'flexidayoff_leave','leave_max_limit' => '8']);
+                         
+
     }
 
     /**
