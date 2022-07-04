fa1d31419a076be69f65a49638827081eae3751d
praveen <voidmaxtech@gmail.com>
2022-07-04 11:50:24 +0530

    Minor updates

--- resources/views/layouts/layouts-vertical-hovered.blade.php
+++ resources/views/layouts/layouts-vertical-hovered.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8" />
     <title>@yield('title') | Velzon - Admin & Dashboard Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-ś
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ URL::asset('assets/images/favicon.ico')}}">
     @include('layouts.head-css')
