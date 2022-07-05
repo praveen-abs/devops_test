288852fdfa9f9d07957843dbbe5a314ab6e41964
george <nijenthen@gmail.com>
2022-07-05 18:02:05 +0530

    pms dashboard created

--- resources/views/vmt_pms_dashboard.blade.php
+++ resources/views/vmt_pms_dashboard.blade.php
@@ -11,52 +11,87 @@
 @endcomponent
 
 <div class="container-fluid pms-dashboard">
-    <div class="card   pms-dashboard-wrapper ">
+    <div class="card px-5 py-2 pms-card-wrapper  pms-dashboard-wrapper ">
 
 
-        <div class="card-body">
-            <div class="row ">
-                <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 ">
+        <div class="card-body p-5">
+            <!-- <div class="row ">
+                <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 "> -->
 
-                    <div class="card pms-card">
-                        <div class="card-body">
-                            <img src="{{ URL::asset('assets/images/comingsoon.png') }}" alt="" class="">
+            <div class="d-flex  flex-wrap align-items-center justify-content-center inside-pms-container">
+                <div class="card pms-card m-0 m-3">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <img src="{{ URL::asset('assets/images/self_review.png') }}" alt="" class="">
+                            <p>Self Review</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 ">
-                    <div class="card pms-card">
-                        <div class="card-body">
-                            <img src="{{ URL::asset('/assets/images/png_icons/goals_assignment.png')}}"
-                                class="img-fluid rounded-start">
+
+                <div class="card pms-card m-0 m-3">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <img src="{{ URL::asset('assets/images/rating_assessment.png') }}" alt="" class="">
+                            <p>Rating assessment reminder notiﬁcation</p>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 ">
-                    <div class="card pms-card">
-                        <div class="card-body">
-                            <img src="{{ URL::asset('/assets/images/png_icons/manager_review.png')}}"
-                                class="img-fluid rounded-start">
+                <!-- <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 "> -->
+                <div class="card pms-card m-0 m-3">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <img src="{{ URL::asset('/assets/images/goals_assignment.png')}}" class="rounded-circle">
+                            <p>Goals assignment reminder notiﬁcation</p>
                         </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+                <!-- <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 "> -->
+                <div class="card pms-card m-0 m-3">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <img src="{{ URL::asset('/assets/images/manager_review.png')}}" class="">
+                            <p>Manager Review</p>
+
+                        </div>
+                        <!-- </div> -->
 
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 ">
-                    <div class="card pms-card">
-                        <div class="card-body">
-                            <img src="{{ URL::asset('/assets/images/png_icons/org_review.png')}}"
-                                class="img-fluid rounded-start">
+                <!-- <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 "> -->
+                <div class="card pms-card m-0 m-3">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <img src="{{ URL::asset('/assets/images/org.png')}}" class="">
+                            <p>Organisation Review</p>
                         </div>
-
                     </div>
+
+                </div>
+                <div class="card pms-card m-0 m-3">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <img src="{{ URL::asset('/assets/images/employee_goals.png')}}" class="">
+                            <p>Employee Goals</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="card pms-card m-0 m-3">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center">
+                            <img src="{{ URL::asset('/assets/images/employees_assessed.png')}}" class="">
+                            <p>Employees Assessed</p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
+
     </div>
-</div>
-</div>
 
-
+</div>
 @endsection
 @section('script')
 
