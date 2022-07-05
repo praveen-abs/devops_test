1a85ddb070aee09fd0ee5e8ab514689d03f97617
george <nijenthen@gmail.com>
2022-07-05 16:33:29 +0530

    for pms icons

--- resources/views/vmt_pms_dashboard.blade.php
+++ resources/views/vmt_pms_dashboard.blade.php
@@ -10,57 +10,59 @@
 @slot('title') Employee Onboarding @endslot
 @endcomponent
 
+<div class="container-fluid pms-dashboard">
+    <div class="card   pms-dashboard-wrapper ">
 
 
+        <div class="card-body">
+            <div class="row ">
+                <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 ">
 
+                    <div class="card pms-card">
+                        <div class="card-body">
+                            <img src="{{ URL::asset('assets/images/png_icons/employees_goals.png') }}" alt="" class="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 ">
+                    <div class="card pms-card">
+                        <div class="card-body">
+                            <img src="{{ URL::asset('/assets/images/png_icons/goals_assignment.png')}}"
+                                class="img-fluid rounded-start">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 ">
+                    <div class="card pms-card">
+                        <div class="card-body">
+                            <img src="{{ URL::asset('/assets/images/png_icons/manager_review.png')}}"
+                                class="img-fluid rounded-start">
+                        </div>
 
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6 col-lg-3 col-xl-3 ">
+                    <div class="card pms-card">
+                        <div class="card-body">
+                            <img src="{{ URL::asset('/assets/images/png_icons/org_review.png')}}"
+                                class="img-fluid rounded-start">
+                        </div>
 
-<!--Font Awesome-->
-<link rel="stylesheet" href="{{ URL::asset('/assets/premassets/css/fontawesome-all.min.css') }}">
-<link rel="stylesheet" href="{{ URL::asset('/assets/premassets/css/fontawesome.css') }}">
-<!--Animate CSS-->
-<link rel="stylesheet" href="{{ URL::asset('/assets/premassets/css/chartist.min.css') }}">
-<!--Map-->
-<link rel="stylesheet" href="{{ URL::asset('/assets/premassets/css/jquery-jvectormap-2.0.2.css') }}">
-
-
-<link href='//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css' rel='stylesheet' type='text/css'>
-
-
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
 
 
 @endsection
-@section('content')
-@component('components.breadcrumb')
-@slot('li_1') Dashboards @endslot
-@slot('title') Dashboard @endslot
-@endcomponent
-
-
-
-
-@endsection
-
 @section('script')
-<!-- Prem assets -->
-<!-- OWL CAROUSEL -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min.js"></script>
 
+<!-- ui notifications -->
 
-<script src="{{ URL::asset('/assets/premassets/js/charts/chart.min.js') }}"></script>
-<script src="{{ URL::asset('/assets/premassets/js/charts/demo.js') }}"></script>
-
-<script src="{{ URL::asset('/assets/premassets/js/maps/jquery-jvectormap-2.0.2.min.js') }}"></script>
-<script src="{{ URL::asset('/assets/premassets/js/maps/jquery-jvectormap-world-mill-en.js') }}"></script>
-<script src="{{ URL::asset('/assets/premassets/js/maps/jvector-maps.js') }}"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.js"></script>
-
-
-
-
-
-
+<script src="{{ URL::asset('/assets/libs/prismjs/prismjs.min.js') }}"></script>
+<script src="{{ URL::asset('/assets/js/pages/notifications.init.js') }}"></script>
+<!-- apexcharts -->
 @endsection
