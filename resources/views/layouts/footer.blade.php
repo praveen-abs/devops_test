b27b1c0a91d9f7bd3d7cfadb0e674dd32f747189
george <nijenthen@gmail.com>
2022-07-12 15:54:45 +0530

    privacy and FAQ added in footer.
    added new file vmt_privacy_policy.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,30 @@
 <footer class="footer">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-6">
+    <div class="container-fluid text-end py-2">
+
+        <div class=" d-flex justify-content-end align-items-center footer-contents py-2 overflow-hidden">
+
+            <div class="d-flex  align-items-center">
+                <span class="powerby-footer w-0 ">Powered By
+                    <img src="{{ URL::asset('assets/images/logo.png') }}" alt="" class="">
+                </span>
+
+
+
+                <a href="{{url('vmt_privacy_policy')}}" class="text-secondary privacy-policy" role="button">
+                    
+
+                        Privacy
+                        Policy
+                    
+                </a>
+
+                <a href="{{url('vmt_privacy_policy')}}" class="text-secondary faq " role="button">
+                    
+                        FAQ
+                    
+                </a>
             </div>
-            <div class="col-sm-6">
-                <div class="text-sm-end d-none d-sm-block">
-                    Powered By 
-                            <img src="{{ URL::asset('assets/images/logo.png') }}" alt="" >
-                </div>
-            </div>
+
         </div>
     </div>
 </footer>
