279aa6b82bd9b00e65a7e8751ec8e4d9da0f1f0f
Praveen <max@gmail.com>
2022-08-03 17:29:03 +0530

    Bug Fix : Counter increment to infinity when counter value is 0

--- resources/views/vmt_hr_dashboard.blade.php
+++ resources/views/vmt_hr_dashboard.blade.php
@@ -562,16 +562,24 @@ $(function() {
     function count($this) {
         var current = parseInt($this.html(), 10);
         $this.html(++current);
-        if (current !== $this.data('count')) {
-            setTimeout(function() {
-                count($this)
-            }, 50);
+
+        if(current != 0)
+        {
+            if (current !== $this.data('count')) {
+                setTimeout(function() {
+                    count($this)
+                }, 50);
+            }
         }
     }
+
     $(".number-increment").each(function() {
-        $(this).data('count', parseInt($(this).html(), 10));
+        var currentValue = parseInt($(this).html(), 10);
+        $(this).data('count', currentValue);
         $(this).html('0');
-        count($(this));
+
+        if(currentValue != 0)
+          count($(this));
     });
 });
 </script>
