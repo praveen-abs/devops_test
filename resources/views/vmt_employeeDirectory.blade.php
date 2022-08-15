48c3ecdbc6e8b7c0b7827a3fc35de105dec31907
Nirmal Raj <nirmlrj@gmail.com>
2022-08-15 15:46:41 +0530

    Calendar added in dashboard page

--- resources/views/vmt_employeeDirectory.blade.php
+++ resources/views/vmt_employeeDirectory.blade.php
@@ -316,7 +316,6 @@
 @section('script')
 <!-- apexcharts -->
 <script src="{{ URL::asset('/assets/js/pages/dashboard-projects.init.js') }}"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
