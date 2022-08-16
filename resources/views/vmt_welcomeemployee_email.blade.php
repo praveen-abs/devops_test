51a9cf8f054ce689c01c365547839be38dae3a6d
hentryfryzen <hhentryfryzen007@gmail.com>
2022-08-16 16:47:22 +0530

    in email template in onboard model is done pending is social button only

--- resources/views/vmt_welcomeemployee_email.blade.php
+++ resources/views/vmt_welcomeemployee_email.blade.php
@@ -12,16 +12,43 @@
 <html lang="en-US">
 
 <head>
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
     <title>New Account Email Template</title>
     <meta name="description" content="New Account Email Template.">
     <style type="text/css">
         a:hover {text-decoration: underline !important;}
+        .button {
+  padding: 1px 10px;
+  font-size: 19px;
+  text-align: center;
+  cursor: pointer;
+  outline: none;
+  color: orange;
+  background-color: orange;
+  
+  border-radius: 15px;
+  
+}
+
+.button:hover {background-color: #3e8e41}
+
+.button:active {
+  background-color: #3e8e41;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
+}
     </style>
 </head>
 
 <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
-	<table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+    <!-- 100% body table -->
+    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
         style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
         <tr>
             <td>
@@ -33,8 +60,7 @@
                     <tr>
                         <td style="text-align:center;">
                             <a href="https://abshrms.com" title="logo" target="_blank">
-                            <img src="data:image/png;base64,{{base64_encode(file_get_contents(public_path('/generalinfo/client-logo.png')))}}"
-                                style="width: 140px; height: 70px;" title="">
+                            <img src="https://demo.abshrms.com/generalinfo/client-logo.png" style="width:140px; height:40px;" alt="">
                           </a>
                         </td>
                     </tr>
@@ -43,20 +69,39 @@
                     </tr>
                     <tr>
                         <td>
-                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
-                                style="max-width:670px; background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0" class="brand-content"
+                                style="max-width:670px; background:#212529; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
                                 <tr>
                                     <td style="height:40px;">&nbsp;</td>
                                 </tr>
                                 <tr>
                                     <td style="padding:0 35px;">
-                                        <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Get started
+                                        <h1 style="color:#fff; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Get started
                                         </h1>
-                                        <p style="font-size:15px; color:#455056; margin:8px 0 0; line-height:24px;">
+                                        <p style="font-size:15px; color:#fff; margin:8px 0 0; line-height:24px;">
                                             ABShrms would like to invite you to join our organisation's employee portal. Using the portal you can access all your payroll related information. You can: <br><strong>Please change
                                                 the password immediately after login</strong>.</p>
-                                        <span
-                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                       <br>
+                                         <button class="button" style="color:#fff;">Get in touch</button>
+
+                                        
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                            </table>
+                            <br>
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                style="max-width:500px; background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                <tr>
+                                    <td style="height:40px;">&nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td style="padding:0 35px;">
+                                      
+                                      <h5 class="card-title fw-bold text-center m-3">Login to your account</h5>
+                <p class="text-start ">Please change the password immediately when you login </p>
                                         <p
                                             style="color:#455056; font-size:18px;line-height:20px; margin:0; font-weight: 500;">
                                             <strong
@@ -65,9 +110,8 @@
                                                 style="display: block; font-size: 13px; margin: 24px 0 4px 0; font-weight:normal; color:rgba(0,0,0,.64);">Password</strong>{{$uPassowrd}}
                                         </p>
 
-                                        <a href="{{$loginLink}}"
-                                            style="background:#20e277;text-decoration:none !important; display:inline-block; font-weight:500; margin-top:24px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Login
-                                            to your Account</a>
+                                        <a href="{{$loginLink}}" style="background:blue;text-decoration:none !important; display:inline-block; font-weight:500; margin-top:24px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;" type="submit">Login</a>
+
                                     </td>
                                 </tr>
                                 <tr>
@@ -81,16 +125,59 @@
                     </tr>
                     <tr>
                         <td style="text-align:center;">
-                            <p style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">&copy; <strong>Disclaimer:
-<b>This e-mail was generated from ABShrms</b>
-<b>If you think this is SPAM, please do report to info@abshrms.com</b>
- </p>
+                           <p class="text-muted"> This e-mail was generated from ABShrms If you think this is SPAM, please do report to
+                <a href="info@abshrms.com">info@abshrms.com</a>
+            </p> 
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css" integrity="sha384-3AB7yXWz4OeoZcPbieVW64vVXEwADiYyAEhwilzWsLw+9FgqpyjjStpPnpBO8o8S" crossorigin="anonymous">
+
+            <!-- Facebook -->
+<i class="fab fa-facebook-f" style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Twitter -->
+<i class="fab fa-twitter"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Google -->
+ <a href="https://www.ardens.in/" class="fab fa-google"style="width: 30px ; height:30px; width:30px; height:30px;"></a>
+
+<!-- Instagram -->
+<i class="fab fa-instagram"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Linkedin -->
+<i class="fab fa-linkedin-in"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Pinterest -->
+<i class="fab fa-pinterest"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Vkontakte -->
+<i class="fab fa-vk"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Stack overflow -->
+<i class="fab fa-stack-overflow"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Youtube -->
+<i class="fab fa-youtube"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Slack -->
+<i class="fab fa-slack-hash"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Github -->
+<i class="fab fa-github"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Dribbble -->
+<i class="fab fa-dribbble"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Reddit -->
+<i class="fab fa-reddit-alien"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
+
+<!-- Whatsapp -->
+<i class="fab fa-whatsapp"style="width: 30px ; height:30px; width:30px; height:30px;"></i>
                         </td>
                     </tr>
                     <tr>
                         <td style="height:80px;">&nbsp;</td>
                     </tr>
                 </table>
+
             </td>
         </tr>
     </table>
@@ -99,4 +186,8 @@
 
 </html>
 <!-- partial -->
+  
+</body>
+</html>
+
   
