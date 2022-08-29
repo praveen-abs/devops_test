308801684bfb96457b934553878448db8bd5700d
Rahul <you@example.com>
2022-08-29 08:47:48 +0530

    PMS Review API : view list , details review for assignee

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::get('viewAssigneeReviewList', 'App\Http\Controllers\VmtAPIPMSModuleController@showEmployeeApraisalReviewList');
+
+    Route::get('viewAssigneeReviews', 'App\Http\Controllers\VmtAPIPMSModuleController@showEmployeeApraisalReview');
+
+    Route::post('saveAssigneeReviews', 'App\Http\Controllers\VmtAPIPMSModuleController@saveEmployeeApraisalReview');
+});
