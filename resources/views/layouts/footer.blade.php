77a6607d2e97f1bce06d6f9e69f346f07e12e518
george <nijenthen@gmail.com>
2022-08-30 04:31:55 -0700

    pdf aligned
    and salary content(vmt_home) aligned

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="d-flex align-items-center p-1 justify-content-end">
         <div class="footer-img-wrapper d-flex align-items-center">
-            <p class="f-12 me-2">Powered By
+            <p class="f-12 me-2 mb-0">Powered By
             </p>
             <img src="{{ URL::asset('assets/images/logo.png') }}" alt="" class="">
         </div>
