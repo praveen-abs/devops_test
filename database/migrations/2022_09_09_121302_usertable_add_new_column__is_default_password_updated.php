bd7545076a8e902b2f2ebb8a0ee25695e8c17cdf
Praveen <max@gmail.com>
2022-09-10 00:49:25 +0530

    Bug Fix : migration

--- database/migrations/2022_09_09_121302_usertable_add_new_column__is_default_password_updated.php
+++ database/migrations/2022_09_09_121302_usertable_add_new_column__is_default_password_updated.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->text('is_default_password_updated')->default('1');
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('Users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('is_default_password_updated');
         });
     }
