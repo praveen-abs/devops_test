43bf144e7ce7d620d497d9f1e357339c104bdb4a
dilip <dilip@mail.com>
2022-09-12 09:29:24 +0530

    added blank pages for attendance modules (approvals, leave, overtime, shift_weeklyoff_holidays, tracking,report, settings
    )

---
+++ resources/views/attendance_tracking.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.master')
+
+@section('title')
+    @lang('translation.dashboards')
+@endsection
+
+@section('css')
+<link href="{{URL::asset('assets/css/shared.css')}}" rel="stylesheet">
+
+<style>
+ .project-wrapper{
+		position: relative;
+		top:-20px;
+    }
+</style>
+@endsection
+
+@section('content')
+ @component('components.attendance_breadcrumb')
+   @slot('li_1')@endslot
+ @endcomponent
+
+<div class="card flex-fill project-wrapper">
+	<div class="card-header">
+	   <h5><span class="text-muted">Attendance &gt;</span> <span class="text-danger">  </span></h5>
+	</div>
+        <div class="card-body p-2">
+
+	</div>
+</div>
+@endsection
+
+@section('script')
+<script>
+</script>
+@endsection
