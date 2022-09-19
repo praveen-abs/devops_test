f89822be72d1cc686fcb38f6248ce8c453c62309
Praveen <praveenmax@users.noreply.github.com>
2022-09-19 19:18:40 +0530

    Custom role based func added.

---
+++ database/migrations/2022_09_19_133358_users__add_new_columns_is_manager_is_hr.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('org_role')->default('employee');//employee,manager,hr
+            $table->smallInteger('is_ssa')->default(0);//Super duper Admin
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('org_role');
+            $table->dropColumn('is_ssa');
+        });
+    }
+};
