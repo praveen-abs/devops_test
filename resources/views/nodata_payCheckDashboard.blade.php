b415688fd415e73626186422375962d107bed6a2
george <nijenthen@gmail.com>
2022-09-26 11:21:06 +0530

    no data content added

--- resources/views/nodata_payCheckDashboard.blade.php
+++ resources/views/nodata_payCheckDashboard.blade.php
@@ -7,17 +7,16 @@
 @endsection
 @section('content')
 @component('components.paycheck_breadcrumb')
-@slot('li_1')  @endslot
+@slot('li_1') @endslot
 @endcomponent
-<div class="investments-wrapper bg-white mt15-mb30">
+<div class="investments-wrapper">
     <div class="card shadow profile-box card-top-border ">
         <div class="fill salary-header nav-tab-header">
             <div class="row">
                 <div class="col-xs-6">
                     <ul class="nav nav-pills nav-tabs-dashed">
                         <li id="ember76" class="active ember-view">
-                            <a href="#/investments-and-proofs/investment-declaration" id="ember77"
-                                class="active ember-view">
+                            <a href="#/investments-and-proofs/investment-declaration" id="ember77" class="active ember-view">
                                 PayCheck
                             </a>
                         </li>
@@ -29,23 +28,16 @@
         <div class="zp-container nav-tab-body-top scroll-x scroll-y">
             <div id="ember83" class="ember-view">
                 <div class="fill body content">
-                    <div class="empty-state-help-centered ind-empty-state">
-                        <img src="https://css.zohostatic.in/zfpayroll/zpayrollportal///assets/it-empty-state-68d45ef7fddba0f96b1ec6b7664ace53.svg"
-                            class="empty-state-image">
+                    <div class="empty-state-help-centered ind-empty-state d-flex align-items-center justify-content-center flex-column ">
+                        <img src="https://css.zohostatic.in/zfpayroll/zpayrollportal///assets/it-empty-state-68d45ef7fddba0f96b1ec6b7664ace53.svg" class="empty-state-image">
                         <div class="empty-state-content">
-                            <div class="empty-state-content-header">
-                                No data
+                            <div class="">
+                                <h5> No data</h5>
                             </div>
-                            <div class="empty-state-content-subtext">
-
-                            </div>
-                            <!---->
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
     </div>
 </div>
