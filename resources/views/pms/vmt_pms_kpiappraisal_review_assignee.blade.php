a7bb42e6f6a68949e1612513f356c8f449f21853
Praveen <praveenmax@users.noreply.github.com>
2022-10-21 13:39:55 +0530

    Bug Fix : PMS review page : Department is shown now

--- resources/views/pms/vmt_pms_kpiappraisal_review_assignee.blade.php
+++ resources/views/pms/vmt_pms_kpiappraisal_review_assignee.blade.php
@@ -120,7 +120,7 @@
                         <div class="card mb-0 w-100 border-0 boxshadow_lite4">
                             <div class="card-body">
                                 <p class="f-14 text-ash  ">Business Unit/Process/Function</p>
-                                <p class="mb-4 f-15 fw-bold text-primary">{{ $assignedUserDetails->getEmployeeOfficeDetails->department }}</p>
+                                <p class="mb-4 f-15 fw-bold text-primary">{{ $assignedUserDetails->getEmployeeOfficeDetails->department_id }}</p>
                                 <p class="f-14 text-ash  ">Reporting Manager</p>
                                 <p class="mb-4 f-15 fw-bold text-primary ">{{ $assignersName }}</p>
                                 <p class="f-14 text-ash  ">Review Period</p>
