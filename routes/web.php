8b7421d8295129a570761ebdcf1d6354b9cd35c8
Praveen <praveenmax@users.noreply.github.com>
2022-10-31 10:13:14 +0530

    Added new attendance route link for attendance_calendar

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,7 @@ Route::controller(VmtEmployeeOnboardingController::class)->group(function () {
 Route::get('/attendance-dashboard', [App\Http\Controllers\VmtAttendanceController::class, 'showDashboard'])->name('attendance-dashboard');
 Route::get('/attendance-leavepolicy', [App\Http\Controllers\VmtAttendanceController::class, 'showAttendanceLeavePolicyPage'])->name('attendance-leavepolicy');
 Route::get('/attendance-leavereports', [App\Http\Controllers\VmtAttendanceController::class, 'showAttendanceLeaveReportsPage'])->name('attendance-leavereports');
+Route::get('/attendance-org-timesheet', [App\Http\Controllers\VmtAttendanceController::class, 'showOrgTimesheet'])->name('attendance-org-timesheet');
 
 //Leave history pages
 Route::get('/attendance-leavehistory/{type}', [App\Http\Controllers\VmtAttendanceController::class, 'showLeaveHistoryPage'])->name('attendance-leavehistory');
