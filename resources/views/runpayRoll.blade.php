75eade37db0f856c2242361900f4b03da26ead5f
george <nijenthen@gmail.com>
2022-11-14 15:34:35 +0530

    minute changes

--- resources/views/runpayRoll.blade.php
+++ resources/views/runpayRoll.blade.php
@@ -154,13 +154,21 @@
                 <div class="row">
                     <div class="col-md-8">
                         <div class="card card-body">
-                            <h6 class="mb-3">Sep - 2022<span class="f-13">(30 Days)</span></h6>
+                            <h6 class="mb-3">Oct - 2022<span class="f-13">(31 Days)</span></h6>
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="card  box_shadow_0 border-rtb left-line w-100">
                                         <div class="card-body text-center">
                                             <p class="text-ash-medium mb-2 f-13">Total Active Employees</p>
-                                            <h5 class="mb-0">-</h5>
+                                            <h5 class="mb-0">152</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="card  box_shadow_0 border-rtb left-line w-100">
+                                        <div class="card-body text-center">
+                                            <p class="text-ash-medium mb-2 f-13">Calendar Days</p>
+                                            <h5 class="mb-0">31</h5>
                                         </div>
                                     </div>
                                 </div>
@@ -168,15 +176,7 @@
                                     <div class="card  box_shadow_0 border-rtb left-line w-100">
                                         <div class="card-body text-center">
                                             <p class="text-ash-medium mb-2 f-13">Total Active Employees</p>
-                                            <h5 class="mb-0">-</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-4">
-                                    <div class="card  box_shadow_0 border-rtb left-line w-100">
-                                        <div class="card-body text-center">
-                                            <p class="text-ash-medium mb-2 f-13">Total Active Employees</p>
-                                            <h5 class="mb-0">-</h5>
+                                            <h5 class="mb-0">152/152</h5>
                                         </div>
                                     </div>
                                 </div>
@@ -186,22 +186,22 @@
                                     <div class="d-flex justify-content-between">
                                         <div class="">
                                             <p class="text-ash-medium mb-2 f-13">Total Payroll Cost</p>
-                                            <h5 class="mb-0">-</h5>
+                                            <h5 class="mb-0">INR 1,15,91,555</h5>
                                         </div>
                                         <div class="f-20">=</div>
                                         <div class="">
                                             <p class="text-ash-medium mb-2 f-13">Employee Deposit</p>
-                                            <h5 class="mb-0">-</h5>
+                                            <h5 class="mb-0">INR 54,00,000</h5>
                                         </div>
                                         <div class="f-20">+</div>
                                         <div class="">
                                             <p class="text-ash-medium mb-2 f-13">Total Deductions</p>
-                                            <h5 class="mb-0">-</h5>
+                                            <h5 class="mb-0">INR 26,91,555</h5>
                                         </div>
                                         <div class="f-20">+</div>
                                         <div class="">
                                             <p class="text-ash-medium mb-2 f-13">Total contributions</p>
-                                            <h5 class="mb-0">-</h5>
+                                            <h5 class="mb-0">INR 35,00,00</h5>
                                         </div>
                                     </div>
 
