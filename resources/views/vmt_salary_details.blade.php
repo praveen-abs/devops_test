39505a7962921c2d1c24265a6fbc6950eb680623
george <nijenthen@gmail.com>
2022-11-21 08:22:20 +0530

    investments page ui completed

--- resources/views/vmt_salary_details.blade.php
+++ resources/views/vmt_salary_details.blade.php
@@ -2,14 +2,10 @@
 @extends('layouts.master')
 @section('css')
 
-<link href="{{ URL::asset('assets/libs/jsvectormap/jsvectormap.min.css') }}" rel="stylesheet">
+
 <link href="{{ URL::asset('public\assets\css\payCheck.css') }}" rel="stylesheet">
 
-<style>
-    .cursor-pointer {
-        cursor: pointer;
-    }
-</style>
+
 @endsection
 @section('content')
 @component('components.paycheck_breadcrumb')
