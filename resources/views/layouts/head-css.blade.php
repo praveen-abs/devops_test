dbab69c0c8620f5a7a6c5725b6ca9c086f7993e9
george <nijenthen@gmail.com>
2022-11-30 18:20:57 +0530

    font awesome cdn added inside head-css
    WIP:
    profile page design completed.
    functionality is pending and pop up

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -24,6 +24,7 @@
 
 <link rel="stylesheet" href="{{ URL::asset('/assets/premassets/css/fontawesome-all.min.css') }}">
 <link rel="stylesheet" href="{{ URL::asset('/assets/premassets/css/fontawesome.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <!-- custom Css-->
 <link href="{{ URL::asset('assets/css/custom.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
