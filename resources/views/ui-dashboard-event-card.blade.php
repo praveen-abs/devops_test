7ef7ed088b6ed024c40689266e3d8d7516e1414f
george <nijenthen@gmail.com>
2022-12-03 01:05:05 +0530

    removed border from modal heading

--- resources/views/ui-dashboard-event-card.blade.php
+++ resources/views/ui-dashboard-event-card.blade.php
@@ -164,8 +164,8 @@
     <div id="wishes_popup" class="modal  fade" role="dialog">
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable  modal-md">
             <div class="modal-content">
-                <div class="modal-header py-2 new-role-header border-0 d-flex align-items-center">
-                    <h6 class="modal-title mb-1 text-primary" style="border-bottom:5px solid #d0d4e2;">
+                <div class="modal-header py-2 border-0">
+                    <h6 class="modal-title " >
                         Wishes Text</h6>
                     <button type="button" class="close outline-none bg-transparent border-0 h3" data-bs-dismiss="modal"
                         aria-label="Close">
