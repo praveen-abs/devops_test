ae9b1daba9334e8cd72fd395c468d7a3806b1fa2
george <nijenthen@gmail.com>
2022-12-23 16:48:47 +0530

    leave policy added to vasa legal entity.
    Fixed:
    slide button background color added to brand avatar leave policy file

--- resources/views/leave_policy_brandavatar.blade.php
+++ resources/views/leave_policy_brandavatar.blade.php
@@ -1080,12 +1080,12 @@
                             </div>
                             <button class="carousel-control-prev" type="button"
                                 data-bs-target="#policy_carousel" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="carousel-control-prev-icon bg-secondary rounded-circle" aria-hidden="true"></span>
                                 <span class="visually-hidden">Previous</span>
                             </button>
                             <button class="carousel-control-next" type="button"
                                 data-bs-target="#policy_carousel" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="carousel-control-next-icon bg-secondary rounded-circle" aria-hidden="true"></span>
                                 <span class="visually-hidden">Next</span>
                             </button>
                         </div>
