de9ecbce73f4c40e1efb1fe8d38ef77fb73c3406
Karthick <test@gmail.com>
2022-12-24 11:37:42 +0530

    Added first row as a bold while exporting Attendance report.

--- app/Exports/EmployeeAttendanceExport.php
+++ app/Exports/EmployeeAttendanceExport.php
@@ -5,8 +5,11 @@ namespace App\Exports;
 use App\Models\VmtEmployeeAttendance;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class EmployeeAttendanceExport implements FromCollection,WithHeadings
+
+class EmployeeAttendanceExport implements FromCollection,WithHeadings,WithStyles
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -21,13 +24,35 @@ class EmployeeAttendanceExport implements FromCollection,WithHeadings
             'Out Punch'
         ];
     }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the first row as bold text.
+            1    => ['font' => ['bold' => true]],
+
+        ];
+    }
+
+
+
     public function collection()
     {
 
+    //     VmtEmployeeAttendance::leftJoin('users as us', 'us.id', '=', 'vmt_employee_attendance.user_id')
+    //    ->leftjoin('vmt_employee_office_details', 'vmt_employee_office_details.user_id', '=','us.id')
+    //    ->get(['user_code','name','designation','checkin_time','checkout_time']);
 
-       return VmtEmployeeAttendance::leftJoin('users as us', 'us.id', '=', 'vmt_employee_attendance.user_id')
-       ->leftjoin('vmt_employee_office_details', 'vmt_employee_office_details.user_id', '=','us.id')
-       ->get(['user_code','name','designation','checkin_time','checkout_time']);
+
+       return VmtEmployeeAttendance::leftJoin('users', 'users.id', '=', 'vmt_employee_attendance.user_id')
+       ->leftjoin('vmt_employee_office_details', 'vmt_employee_office_details.user_id', '=','users.id')
+       ->get(['users.user_code',
+              'users.name',
+              'vmt_employee_office_details.designation',
+              'vmt_employee_attendance.checkin_time',
+              'vmt_employee_attendance.checkout_time'
+              ]
+        );
 
     }
 }
