6a59e7110eb73fc92f4232ed8278224d84a6ffc4
Karthick <test@gmail.com>
2022-12-25 18:56:46 +0530

    WIP : Attendance Excel report generation

--- app/Exports/EmployeeAttendanceExport.php
+++ app/Exports/EmployeeAttendanceExport.php
@@ -2,11 +2,13 @@
 
 namespace App\Exports;
 
+use App\Models\User;
 use App\Models\VmtEmployeeAttendance;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Carbon\Carbon;
 
 
 class EmployeeAttendanceExport implements FromCollection,WithHeadings,WithStyles
@@ -44,15 +46,55 @@ class EmployeeAttendanceExport implements FromCollection,WithHeadings,WithStyles
     //    ->get(['user_code','name','designation','checkin_time','checkout_time']);
 
 
-       return VmtEmployeeAttendance::leftJoin('users', 'users.id', '=', 'vmt_employee_attendance.user_id')
+       $attendance_details = VmtEmployeeAttendance::leftJoin('users', 'users.id', '=', 'vmt_employee_attendance.user_id')
        ->leftjoin('vmt_employee_office_details', 'vmt_employee_office_details.user_id', '=','users.id')
-       ->get(['users.user_code',
+       ->get([
+              'users.user_code',
               'users.name',
               'vmt_employee_office_details.designation',
+              'vmt_employee_attendance.date',
               'vmt_employee_attendance.checkin_time',
-              'vmt_employee_attendance.checkout_time'
+              'vmt_employee_attendance.checkout_time',
               ]
         );
 
+        $month = 12;
+
+        $month_days_count = 31;
+
+        $employee_list = User::where('is_ssa','0')
+                    ->where('active','1')
+                    ->get(['name','user_code']);
+
+       // dd($employee_list);
+
+        //for each user
+        foreach($employee_list as $singleEmployee)
+        {
+            for($i = 1 ; $i <= $month_days_count ; $i++){
+
+
+            }
+        }
+        //For each day in a month, find the attendance details
+
+
+        /*
+        foreach($attendance_details as $singleRecord)
+        {
+            //STATUS check : (P,A,etc)
+
+
+            $checkin_time = new Carbon($singleRecord->checkin_time);
+            $checkout_time = new Carbon($singleRecord->checkout_time);
+
+            $duration = $checkin_time->diff($checkout_time)->format('%H:%I');
+
+            $singleRecord["duration"] = $duration;
+        }
+        dd($attendance_details);
+
+        return $attendance_details;
+        */
     }
 }
