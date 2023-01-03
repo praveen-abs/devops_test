33a93dc509909e9e761387efa22afbabb1c74348
Naga <nagaraj.vml472000@gmail.com>
2023-01-03 17:43:46 +0530

    Bug Fix : PMS flow 3 Reviewer reject mail sent

--- app/Mail/VmtPMSMail_Assignee.php
+++ app/Mail/VmtPMSMail_Assignee.php
@@ -17,15 +17,17 @@ class VmtPMSMail_Assignee extends Mailable
      * @return void
      */
     // protected $linkUri;
-    public function __construct( $approvalStatus,$user_emp_name,$appraisal_period,$user_manager_name,$command_emp)
+    public function __construct( $approvalStatus,$flowType, $user_emp_name,$appraisal_period,$user_manager_name,$comments_employee,$loginLink)
     {
         //
 
         $this->approvalStatus = $approvalStatus;
+        $this->flowType = $flowType;// PMS flow 1 / 2 / 3
         $this->user_emp_name = $user_emp_name;
         $this->appraisal_period = $appraisal_period;
         $this->user_manager_name = $user_manager_name;
-        $this->command_emp = $command_emp;
+        $this->comments_employee = $comments_employee;
+        $this->loginLink = $loginLink;
 
     }
 
@@ -40,13 +42,31 @@ class VmtPMSMail_Assignee extends Mailable
         $MAIL_FROM_ADDRESS = env('MAIL_FROM_ADDRESS');
         $MAIL_FROM_NAME    = env('MAIL_FROM_NAME');
 
+        $mail_subject = "---";
+
+        if($this->flowType == "2")
+        {
+            if($this->approvalStatus == "accepted")
+                $mail_subject = "Accepted of OKR/PMS for the Period of " . $this->appraisal_period;
+            else
+                $mail_subject = "Rejected of OKR/PMS for the Period of " . $this->appraisal_period;
+        }
+        else
+        if($this->flowType == "3")
+        {
+            $mail_subject = "Successful Update of PMS/OKR for the Period of " . $this->appraisal_period;
+        }
+
+
         return $this->from($MAIL_FROM_ADDRESS,  $MAIL_FROM_NAME)
-                ->subject($MAIL_FROM_NAME)
+                //Rejected of OKR/PMS for the Period of {Month Name/ Quarter Name/ Half Year Name}
+                ->subject($mail_subject)
                 ->view('vmt_pms_mail_flow_assignee_to_manager')
                 ->with('user_emp_name', $this->user_emp_name)
                 ->with('approvalStatus', $this->approvalStatus)
                 ->with('appraisal_period', $this->appraisal_period)
                 ->with('user_manager_name', $this->user_manager_name)
-                ->with('command_emp', $this->command_emp);
+                ->with('comments_employee', $this->comments_employee)
+                ->with('loginLink', $this->loginLink);
     }
 }
