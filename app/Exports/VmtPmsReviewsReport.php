c7805d123c211a0cb2cfd7a3b446fa35d47fb1df
Karthick <test@gmail.com>
2023-01-06 18:03:17 +0530

    WIP:Logic For Monthly assignment_period and ,Added color for first row

--- app/Exports/VmtPmsReviewsReport.php
+++ app/Exports/VmtPmsReviewsReport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\VmtPMS_KPIFormReviewsModel;
+use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithColumnWidths;
@@ -13,7 +14,12 @@ use Maatwebsite\Excel\Events\AfterSheet;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Sheet;
 use Maatwebsite\Excel\Concerns\FromQuery;
-use App\Models\User;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Style;
+use PhpOffice\PhpSpreadsheet\Style\Color;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+
+
 
 
 
@@ -42,7 +48,7 @@ class VmtPmsReviewsReport implements FromCollection,WithHeadings,WithStyles,With
 
 
 
-        if($is_assignee_submitted==1 || $is_assignee_submitted==0 ){
+        if($is_assignee_submitted==1 ){
             $this->is_assignee_submitted=$is_assignee_submitted;
         }else{
             $this->is_assignee_submitted=null;
@@ -81,6 +87,17 @@ class VmtPmsReviewsReport implements FromCollection,WithHeadings,WithStyles,With
                     'alignment' => [
                         'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER,
                     ],
+                    'borders' => [
+                        'outline' => [
+                            'borderStyle' => Border::BORDER_THICK,
+                            'color' => array('argb' => '00000000'),
+                        ],
+                    ],
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'startColor' => array('argb' => 'ffff31')
+                        ]
+
                 ];
                 $cellRange = 'A1:I1'; // All headers
                 $event->sheet->getDelegate()->getStyle($cellRange)->applyFromArray($styleArray);
@@ -134,7 +151,6 @@ class VmtPmsReviewsReport implements FromCollection,WithHeadings,WithStyles,With
                             ['vmt_pms_kpiform_assigned.calendar_type','=',$this->calendar_type],
                             ['vmt_pms_kpiform_assigned.year','=',$this->year],
                             ['vmt_pms_kpiform_assigned.assignment_period','=',$this->assignment_period],
-                            //['vmt_pms_kpiform_assigned.assignment_period','=','dec'],
                             //['vmt_pms_kpiform_reviews.is_assignee_submitted','=',$this->is_assignee_submitted]
                         ])
                         ->select(
@@ -163,32 +179,78 @@ class VmtPmsReviewsReport implements FromCollection,WithHeadings,WithStyles,With
             }
             if($singleData->calendar_type=="financial_year"){
                 $singleData->calendar_type="Financial Year";
+                if($singleData->frequency=='quarterly'){
+                    $singleData->frequency='Quarterly';
+                    $start_year=substr($singleData->year,8,4);
+                    $end_year=substr($singleData->year,24,4);
+                    if($singleData->assignment_period == "q1"){
+                        $singleData->assignment_period = "Q1 (Apr-".$start_year." - Jun-".$start_year .")";
+                    }
+                    else if($singleData->assignment_period == "q2")
+                    {
+                        $singleData->assignment_period = "Q2 (July-".$start_year." - Sept-".$start_year .")";
+                    }
+                    else if($singleData->assignment_period == "q3")
+                    {
+                        $singleData->assignment_period = "Q3 (Oct-".$start_year." - Dec-".$start_year .")";
+                    }else if($singleData->assignment_period == "q4")
+                    {
+                        $singleData->assignment_period = "Q4 (Jan-".$end_year." - March-".$end_year .")";
+                    }
+                }else if($singleData->frequency=='monthly'){
+                    $singleData->frequency='Monthly';
+                    $month=$singleData->assignment_period;
+                    switch($month){
+                        case "apr":
+                            $singleData->assignment_period = "April - ".$start_year;
+                            break;
+                        case "may":
+                            $singleData->assignment_period = "May - ".$start_year;
+                            break;
+                        case "june":
+                             $singleData->assignment_period = "June - ".$start_year;
+                            break;
+                        case "july":
+                            $singleData->assignment_period = "July - ".$start_year;
+                            break;
+                        case "aug":
+                            $singleData->assignment_period = "August - ".$start_year;
+                            break;
+                        case "sept":
+                            $singleData->assignment_period = "September - ".$start_year;
+                            break;
+                        case "oct":
+                            $singleData->assignment_period = "October - ".$start_year;
+                            break;
+                        case "nov":
+                             $singleData->assignment_period = "November - ".$start_year;
+                            break;
+                        case "dec":
+                            $singleData->assignment_period = "December - ".$start_year;
+                            break;
+                        case "jan":
+                            $singleData->assignment_period = "January - ".$end_year;
+                            break;
+                        case "feb":
+                            $singleData->assignment_period = "February - ".$end_year;
+                            break;
+                        case "mar":
+                            $singleData->assignment_period = "March - ".$end_year;
+                            break;
+
+
+
+
+                    }
+                }else if($singleData->frequency=='yearly'){
+
+                }
+
             }else if($singleData->calendar_type=="calendar_year"){
                 $singleData->calendar_type="Calendar Year";
             }
 
 
-            if($singleData->frequency=='quarterly'){
-                $singleData->frequency='Quarterly';
-                $start_year=substr($singleData->year,8,4);
-                $end_year=substr($singleData->year,24,4);
-                if($singleData->assignment_period == "q1"){
-                    $singleData->assignment_period = "Q1 (Apr-".$start_year." - Jun-".$start_year .")";
-                }
-                else if($singleData->assignment_period == "q2")
-                {
-                    $singleData->assignment_period = "Q2 (July-".$start_year." - Sept-".$start_year .")";
-                }
-                else if($singleData->assignment_period == "q3")
-                {
-                    $singleData->assignment_period = "Q3 (Oct-".$start_year." - Dec-".$start_year .")";
-                }else if($singleData->assignment_period == "q4")
-                {
-                    $singleData->assignment_period = "Q4 (Jan-".$end_year." - March-".$end_year .")";
-                }
-            }else if($singleData->frequency=='monthly'){
-                $singleData->frequency='Monthly';
-            }
 
 
 
