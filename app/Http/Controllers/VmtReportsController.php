584bd584d246d03353d5680895edca49ffe6a558
Karthick <test@gmail.com>
2023-01-06 19:21:00 +0530

    WIP:Pms Report

--- app/Http/Controllers/VmtReportsController.php
+++ app/Http/Controllers/VmtReportsController.php
@@ -31,15 +31,16 @@ class VmtReportsController extends Controller
 
     public function showPmsReviewsReportPage(Request $request){
         $query_configPms= ConfigPms::first();
-        //$query_year= VmtPMS_KPIFormAssignedModel::all('year')->groupby('year');
-        return view('reports.vmt_showPmsReviewsReports', compact('query_configPms'));
-        //dd($query_year);
+        $query_years= VmtPMS_KPIFormAssignedModel::groupby('year')->select('year')->get();
+        print($query_years->value('year'));exit;
+        return view('reports.vmt_showPmsReviewsReports', compact('query_configPms','query_years'));
+
     }
 
     public function generatePmsReviewsReports(Request $request){
         //$filename = 'PmsReports_'.$request->calender_type.'.xlsx';
 
-        
+
         return Excel::download(new VmtPmsReviewsReport($request->calender_type,
                                                        $request->year,
                                                        $request->assignment_period,
