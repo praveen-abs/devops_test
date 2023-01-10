a0741ef8440164216575a6597b94c8bb675157c2
Karthick <test@gmail.com>
2023-01-10 11:41:24 +0530

    WIP:Pms Report

--- app/Http/Controllers/VmtReportsController.php
+++ app/Http/Controllers/VmtReportsController.php
@@ -33,13 +33,36 @@ class VmtReportsController extends Controller
     public function showPmsReviewsReportPage(Request $request){
         $query_configPms= ConfigPms::first(['calendar_type','frequency']);
         $query_years= VmtPMS_KPIFormAssignedModel::groupby('year')->pluck('year');
+        $query_pms_data=VmtPMS_KPIFormReviewsModel::
+        leftJoin('users','users.id', '=','vmt_pms_kpiform_reviews.assignee_id')
+        ->leftJoin('vmt_pms_kpiform_assigned','vmt_pms_kpiform_assigned.id', '=', 'vmt_pms_kpiform_reviews.vmt_pms_kpiform_assigned_id')
+        // ->where([
+        //     //['vmt_pms_kpiform_assigned.calendar_type','=',$this->calendar_type],
+        //     ['vmt_pms_kpiform_assigned.year','=',$this->year],
+        //     ['vmt_pms_kpiform_assigned.assignment_period','=',$this->assignment_period],
+        //     //['vmt_pms_kpiform_reviews.is_assignee_submitted','=',$this->is_assignee_submitted]
+        // ])
+        ->select(
+                  'users.user_code',
+                  'users.name',
+                  'vmt_pms_kpiform_assigned.calendar_type',
+                  'vmt_pms_kpiform_assigned.year',
+                  'vmt_pms_kpiform_assigned.frequency',
+                  'vmt_pms_kpiform_assigned.assignment_period',
+                  'vmt_pms_kpiform_reviews.is_assignee_submitted',
+                   //'vmt_pms_kpiform_reviews.is_reviewer_accepted', //For Manager name
+                   'vmt_pms_kpiform_reviews.is_reviewer_submitted',
 
+
+                 )
+        ->get();
         //dd($query_years);
         //dd($query_years->value('year'));
 
-        return view('reports.vmt_showPmsReviewsReports', compact('query_configPms','query_years','query_configPms'));
+        return view('reports.vmt_showPmsReviewsReports', compact('query_configPms','query_years','query_pms_data'));
 
     }
+
 
     public function generatePmsReviewsReports(Request $request){
         //$filename = 'PmsReports_'.$request->calender_type.'.xlsx';
--- resources/views/reports/vmt_showPmsReviewsReports.blade.php
+++ resources/views/reports/vmt_showPmsReviewsReports.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.master')
 @section('css')
     <link href="{{ URL::asset('assets/libs/jsvectormap/jsvectormap.min.css') }}" rel="stylesheet">
+    {{-- <style>
+        .APR.d-block{
+            display:block !important;
+        }
+    </style> --}}
 @endsection
 
 @section('content')
@@ -13,13 +18,13 @@
                 <span>
                     <b>Calendar Year : </b>
                     <?php
-                        if($query_configPms->calendar_type == "financial_year")
-                            echo "Financial Year";
-                        else
-                        if($query_configPms->calendar_type == "calendar_year")
-                            echo "Calendar Year";
-                        else
-                            echo "Error";
+                    if ($query_configPms->calendar_type == 'financial_year') {
+                        echo 'Financial Year';
+                    } elseif ($query_configPms->calendar_type == 'calendar_year') {
+                        echo 'Calendar Year';
+                    } else {
+                        echo 'Error';
+                    }
                     ?>
                 </span>
             </div>
@@ -28,14 +33,17 @@
                 <span>
                     <b>Year </b>
                     {{-- <input type="text" id="year" readonly value="{{ $query_configPms-> year}}" size="25" /> --}}
-                    <select id="year" class="form-select form-select-sm" style="width:auto;" aria-label=".form-select-sm example">
-                        @foreach($query_years as $key => $value)
-                            <option value="{{ $value }}"> {{$value}} </option>
+                    <select id="year" class="form-select form-select-sm" style="width:auto;"
+                        aria-label=".form-select-sm example">
+                        @foreach ($query_years as $key => $value)
+                            <option value="{{ $value }}"> {{ $value }} </option>
                         @endforeach
                     </select>
 
                 </span>
             </div>
+
+
             <div class=" text-start mb-2">
                 <span>
                     <b>Frequency : </b>{{ ucfirst($query_configPms->frequency) }}
@@ -48,8 +56,8 @@
                     <select placeholder="Select Calendar Type" style="width:auto;" aria-label=".form-select-sm example"
                         id="dropdownAssignment_period" class="form-select form-select-sm">
                         <option value="" selected>Select</option>
-                        @if($query_configPms->calendar_type == "financial_year")
-                            @if($query_configPms->frequency == "monthly")
+                        @if ($query_configPms->calendar_type == 'financial_year')
+                            @if ($query_configPms->frequency == 'monthly')
                                 <option value="apr">Apr</option>
                                 <option value="may">May</option>
                                 <option value="jun">Jun</option>
@@ -62,20 +70,19 @@
                                 <option value="jan">Jan</option>
                                 <option value="feb">Feb</option>
                                 <option value="mar">Mar</option>
-                            @elseif($query_configPms->frequency == "quarterly")
-                                <option  name="Q1" value="q1"> Q1 (Apr-Jun)</option>
-                                <option  name="Q2" value="q2"> Q2 (Jul-Sept)</option>
-                                <option  name="Q3" value="q3"> Q3 (Oct-Dec)</option>
-                                <option  name="Q4" value="q4"> Q4 (Jan-March)</option>
-                            @elseif($query_configPms->frequency == "halfYearly")
-                                <option  name="H1" value="h1">H1 (Apr-Sept)</option>
-                                <option  name="H2" value="h2">H2 (Oct-Mar)</option>
-
-                            @elseif($query_configPms->frequency == "annually")
+                            @elseif($query_configPms->frequency == 'quarterly')
+                                <option name="Q1" value="q1"> Q1 (Apr-Jun)</option>
+                                <option name="Q2" value="q2"> Q2 (Jul-Sept)</option>
+                                <option name="Q3" value="q3"> Q3 (Oct-Dec)</option>
+                                <option name="Q4" value="q4"> Q4 (Jan-March)</option>
+                            @elseif($query_configPms->frequency == 'halfYearly')
+                                <option name="H1" value="h1">H1 (Apr-Sept)</option>
+                                <option name="H2" value="h2">H2 (Oct-Mar)</option>
+                            @elseif($query_configPms->frequency == 'annually')
                                 <option name="A" value="A">A (Apr-Mar)</option>
                             @endif
-                        @elseif($query_configPms->calendar_type == "calendar_year")
-                            @if($query_configPms->frequency == "monthly")
+                        @elseif($query_configPms->calendar_type == 'calendar_year')
+                            @if ($query_configPms->frequency == 'monthly')
                                 <option value="jan">Jan</option>
                                 <option value="feb">Feb</option>
                                 <option value="mar">Mar</option>
@@ -88,15 +95,15 @@
                                 <option value="oct">Oct</option>
                                 <option value="nov">Nov</option>
                                 <option value="dec">Dec</option>
-                            @elseif($query_configPms->frequency == "quarterly")
-                                <option  name="Q1" value="q1"> Q1 (Jan-March)</option>
-                                <option  name="Q2" value="q2"> Q2 (Apr-Jun)</option>
-                                <option  name="Q3" value="q3"> Q3 (Jul-Sept)</option>
-                                <option  name="Q4" value="q4"> Q4 (Oct-Dec)</option>
-                            @elseif($query_configPms->frequency == "halfYearly")
-                                <option  name="H1" value="h1">H1 (Jan-Jun)</option>
-                                <option  name="H2" value="h2">H2 (Jul-Dec)</option>
-                            @elseif($query_configPms->frequency == "annually")
+                            @elseif($query_configPms->frequency == 'quarterly')
+                                <option name="Q1" value="q1"> Q1 (Jan-March)</option>
+                                <option name="Q2" value="q2"> Q2 (Apr-Jun)</option>
+                                <option name="Q3" value="q3"> Q3 (Jul-Sept)</option>
+                                <option name="Q4" value="q4"> Q4 (Oct-Dec)</option>
+                            @elseif($query_configPms->frequency == 'halfYearly')
+                                <option name="H1" value="h1">H1 (Jan-Jun)</option>
+                                <option name="H2" value="h2">H2 (Jul-Dec)</option>
+                            @elseif($query_configPms->frequency == 'annually')
                                 <option name="A" value="A">A (Jan-Dec)</option>
                             @endif
                         @endif
@@ -121,9 +128,74 @@
             <div class=" text-end mb-2">
                 <button class="btn btn-orange me-2" id="btn_downloadReport">Download Report</button>
             </div>
+            <div class="table-responsive">
+                <table id="pmsReportTable" class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Emp Code</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Calendar Type</th>
+                            <th scope="col">Year</th>
+                            <th scope="col">Frequency</th>
+                            <th scope="col">Assignment Period</th>
+                            <th scope="col">Employees Submission Status</th>
+                            <th scope="col">Manager Review Status</th>
+                            <th scope="col">Manager Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        @foreach ($query_pms_data as $query_pms_data)
+                            <?php
+                            $year = $query_pms_data->year;
+
+                            // if ($year == 'April - 2022 to March - 2023'){
+                            //     $year_class = 'APR';
+                            // }
+                            // else{
+                            //     $year_class = 'd-none';
+                            // }
+
+                            $ass_data = $query_pms_data->assignment_period;
+                            ?>
+                            {{-- {{$ass_data = $query_pms_data->assignment_period  }} --}}
+                            {{-- <tr class="<?php echo $year_class . ' ' . $ass_data; ?> d-none"> --}}
+                            <tr>
+                                <td class="userCode">
+                                    {{ $query_pms_data->user_code }}
+                                </td>
+                                <td class="empName">
+                                    {{ $query_pms_data->name }}
+                                </td>
+                                <td class="tableCalendarType">
+                                    {{ $query_pms_data->calendar_type }}
+                                </td>
+                                <td class="year">
+                                    {{ $query_pms_data->year }}
+                                </td>
+                                <td class="frequency">
+                                    {{ $query_pms_data->frequency }}
+                                </td>
+                                <td class="assignmentPeriod">
+                                    {{ $query_pms_data->assignment_period }}
+                                </td>
+                                <td class="assigneeSubmiited">
+                                    {{ $query_pms_data->is_assignee_submitted }}
+                                </td>
+                                <td class="reviewerSubmitted">
+                                    {{ $query_pms_data->is_reviewer_submitted }}
+                                </td>
+                                <td class="managerName">
+                                    {{ $query_pms_data->is_reviewer_submitted }}
+                                </td>
+                            </tr>
+                        @endforeach
 
 
 
+                    </tbody>
+                </table>
+            </div>
             <div class="vendor-table-wrapper">
                 <div id="employee-table" class="noCustomize_gridjs"></div>
             </div>
@@ -136,7 +208,7 @@
     <script>
         $(document).ready(function() {
 
-            let calenderType ='{{ $query_configPms->calendar_type }}';
+            let calenderType = '{{ $query_configPms->calendar_type }}';
 
 
             let year = $('#year').val();
@@ -171,7 +243,7 @@
                 console.log("Download payroll reports....");
                 let selectedAssingementPeriod = $('#dropdownAssignment_period').find(":selected").val();
                 let selectedSubmittedDropdown = $('#dropdownSubmittedStatus').find(":selected").val();
-                console.log(selectedAssingementPeriod + ' '+ selectedSubmittedDropdown);
+                console.log(selectedAssingementPeriod + ' ' + selectedSubmittedDropdown);
                 let URL = '/reports/generatePmsReviewsReports?calender_type=' + calenderType + '&year=' +
                     year + '&assignment_period=' + selectedAssingementPeriod + '&is_assignee_submitted=' +
                     selectedSubmittedDropdown +
@@ -185,29 +257,130 @@
 
             });
 
-            // $('#dropdownAssignment_period').on('change', function() {
-            //     console.log($(this).find(":selected").val());
-            //     var sub = $('#dropdownAssignment_period');
+            var tableCalendarType = document.getElementsByClassName("tableCalendarType");
+            if (tableCalendarType[0].innerText == "financial_year") {
+                console.log(tableCalendarType[0].innerText);
+                for (var i = 0; i < tableCalendarType.length; i++) {
+                    tableCalendarType[i].innerText = "Financial Year";
+                }
+            }
 
-            //         sub.find('option').not(':first').hide();
+            var assigneeSubmiited = document.getElementsByClassName("assigneeSubmiited");
+            for (var i = 0; i < assigneeSubmiited.length; i++) {
+                if (assigneeSubmiited[i].innerText == 1) {
+                    assigneeSubmiited[i].innerText = "Submitted";
+                } else {
+                    assigneeSubmiited[i].innerText = "Not Yet Submitted";
+                }
+            }
 
-            //         $('option', sub).filter(function() {
-            //                 console.log("Assign Period"+calenderType);
-            //             if ($(this).attr('data-group') == calenderType) {
-            //                 $(this).show();
+            var frequency = document.getElementsByClassName("frequency");
+            for (var i = 0; i < frequency.length; i++) {
+                if (frequency[i].innerText == "quarterly") {
+                    frequency[i].innerText = 'Quarterly'
+                    var assignmentPeriod = document.getElementsByClassName("assignmentPeriod");
+                    if (assignmentPeriod[i].innerText == 'q1') {
+                        assignmentPeriod[i].innerText = "Q1 (Apr-Jun)"
+                    } else if (assignmentPeriod[i].innerText == 'q2') {
+                        assignmentPeriod[i].innerText = "Q1 (Jul-Sep)"
+                    } else if (assignmentPeriod[i].innerText == 'q3') {
+                        assignmentPeriod[i].innerText = "Q3 (Oct-Dec)"
+                    } else if (assignmentPeriod[i].innerText == 'q4') {
+                        assignmentPeriod[i].innerText = "Q4 (Jan-Mar)"
+                    }
 
-            //             }
-            //         });
 
+                } else if (frequency[i].innerText == "monthly") {
+                    frequency[i].innerText = 'Monthly'
+                }
+            }
 
-            //         $('#dropdownCalender_year').trigger('change');
+            var reviewerstatus = document.getElementsByClassName("reviewerSubmitted");
+            console.log(reviewerstatus[0].innerText);
+            // $('#dropdownAssignment_period').on("change", function() {
+            //     assign_per = $(this).val();
+            //     console.log(assign_per);
+            //     table = document.getElementById("pmsReportTable");
+            //     tr = table.getElementsByTagName("tr");
+            //     var col_asign = document.getElementsByClassName("assignmentPeriod");
+            //     console.log(col_asign[0].innerText);
+            //     for (i = 0; i < tr.length; i++) {
+            //         if (col_asign[i].innerText == assign_per) {
+            //             tr[i].style.display = "block";
+            //         } else {
+            //             tr[i].style.display = "none";
+            //         }
+            //     }
 
             // });
+            // table = document.getElementById("pmsReportTable");
+            // tr = table.getElementsByTagName("tr");
+            // for(i = 1; i < tr.length; i++){
+            //     var rowcalendar  = tr[i].getElementsByTagName("td")[2].textContent.toUpperCase();
+            //           if(rowcalendar=='financial_year'){
+            //             rowcalendar= 'Financial Year';
+            //           }
+            // }
 
-
+            // $('#dropdownAssignment_period').change(function(){
+            //     var data = $(this).val();
+            //     console.log($('#dropdownAssignment_period').find(`.${data}`),"------------")
+            //     $('#pmsReportTable').find(`.${data}`).addClass('d-block')
+            //     // console.log(data)
+            //     // $(`.${data}`).each(function (index, element) {
+            //     //     console.log(this)
+            //     // });
+            // })
 
 
 
         });
     </script>
+
+
+
+
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.3.2/js/buttons.print.min.js"></script>
+
+    {{-- <script>
+        var year = $('#year').val();
+        var calender = $('#dropdownAssignment_period').val();
+        var formData = 'year='+year+'&calender='+calender;
+        console.log(formData)
+        var ajax_call
+
+        $.get('report_data',formData,function(res){
+            ajax_call = res;
+        })
+
+
+        $(document).ready(function() {
+            setTimeout(() => {
+                console.log(ajax_call,"-----")
+
+                $('#ex').DataTable( {
+                    dom: 'lBfrtip',
+                    buttons: [
+                        'excel', 'pdf', 'print'
+                    ],
+                    "ajax": ajax_call,
+                    "columns": [
+                        {"data": "user_code" },
+                        {"data": "name" },
+                        {"data": "calendar_type" },
+                        {"data": "year" },
+                        {"data": "frequency" },
+                        {"data": "assignment_period" },
+                        {"data": "is_reviewer_submitted" },
+                    ]
+                } );
+            }, 1000);
+} );
+    </script> --}}
 @endsection
