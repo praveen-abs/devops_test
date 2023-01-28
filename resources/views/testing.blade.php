4670c092dadacc7dd0c2e0ae791dc12fc101528c
Praveen <praveenmax@users.noreply.github.com>
2023-01-28 11:48:30 +0530

    VJS : Profile Pages : Docs upload base code added

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -1,3 +1,22 @@
+
+
+<html>
+    <head>
+        <link href="https://unpkg.com/primevue@^3/resources/themes/saga-blue/theme.css" rel="stylesheet" />
+        <link href="https://unpkg.com/primevue@^3/resources/primevue.min.css" rel="stylesheet" />
+        <link href="https://unpkg.com/primeflex@^3/primeflex.min.css" rel="stylesheet" />
+        <link href="https://unpkg.com/primeicons/primeicons.css" rel="stylesheet" />
+
+        @vite(['resources/js/hrms/modules/profile_pages/EmployeeDocumentsManager.js'])
+
+    </head>
+    <body>
+        <div id="vjs_employeeDocsManager">
+        </div>
+    </body>
+</html>
+
 <?php
 	dd(calculateOverallReviewRating('145'));
 ?>
+
