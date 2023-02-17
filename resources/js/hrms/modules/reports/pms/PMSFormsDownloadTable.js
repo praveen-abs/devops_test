623fedcad46855c588fe6e78a1bd6e61336ac7d1
Praveen <praveenmax@users.noreply.github.com>
2023-02-17 19:28:17 +0530

    PMS Form Download : Added datatable.
    TODO :  Need to write ajax code.

--- resources/js/hrms/modules/reports/pms/PMSFormsDownloadTable.js
+++ resources/js/hrms/modules/reports/pms/PMSFormsDownloadTable.js
@@ -9,19 +9,25 @@ import PrimeVue from "primevue/config";
 import BadgeDirective from "primevue/badgedirective";
 import BlockUI from 'primevue/blockui';
 import Button from 'primevue/button';
-import ConfirmationService from 'primevue/confirmationservice';
-import DialogService from 'primevue/dialogservice'
 import FocusTrap from 'primevue/focustrap';
 import Ripple from 'primevue/ripple';
 import StyleClass from 'primevue/styleclass';
-import ToastService from 'primevue/toastservice';
 import Tooltip from 'primevue/tooltip';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import ColumnGroup from 'primevue/columngroup';     //optional for column grouping
-import Row from 'primevue/row';
+import ConfirmDialog from 'primevue/confirmdialog';
+import DialogService from 'primevue/dialogservice';
+import Toast from 'primevue/toast';
+import Dialog from 'primevue/dialog';
+import Dropdown from 'primevue/dropdown';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+import ProgressSpinner from 'primevue/progressspinner';
+import InputText from 'primevue/inputtext'
+import Row from 'primevue/row'
+import ColumnGroup from 'primevue/columngroup'
 
-import PMSFormsDownloadTable from './PMSFormsDownloadTable.vue';
+import PMSFormsDownloadTable from './PMSFormsDownloadTable.vue'
 
 const app = createApp(PMSFormsDownloadTable);
 
@@ -30,14 +36,24 @@ app.use(ConfirmationService);
 app.use(ToastService);
 app.use(DialogService);
 
+
 app.directive('tooltip', Tooltip);
 app.directive('badge', BadgeDirective);
 app.directive('ripple', Ripple);
 app.directive('styleclass', StyleClass);
 app.directive('focustrap', FocusTrap);
 
+app.component('Button', Button);
 app.component('DataTable', DataTable);
 app.component('Column', Column);
+app.component('ColumnGroup', ColumnGroup)
+app.component('Row', Row)
+app.component('Toast', Toast);
+app.component('ConfirmDialog',ConfirmDialog);
+app.component('Dropdown',Dropdown);
+app.component('InputText', InputText);
+app.component('Dialog', Dialog);
+app.component('ProgressSpinner', ProgressSpinner)
 
 app.mount("#vjs_PMSFormsDownloadTable");
 
--- resources/views/reports/pms_reports/vmt_pmsforms_page.blade.php
+++ resources/views/reports/pms_reports/vmt_pmsforms_page.blade.php
@@ -1,22 +1,4 @@
 @extends('layouts.master')
-@section('css')
-    <link href="{{ URL::asset('assets/libs/jsvectormap/jsvectormap.min.css') }}" rel="stylesheet">
-
-    <style>
-        .dt-button {
-            transition: 0.4s ease-in;
-            font-weight: 600;
-            padding: 5px 8px;
-            font-size: 13px;
-            border: 1px solid #ee6a04;
-            background-color: transparent;
-            color: #ee6a04;
-            border-radius: 5px;
-            margin: 5px 5px 0px 0px;
-            float: right;
-        }
-    </style>
-@endsection
 
 @section('content')
 <div class="card mb-0 approvals_wrapper mt-30">
