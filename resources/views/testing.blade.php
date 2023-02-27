c72e3594eb7ececad071c5722c34a3915c987d16
AntonyXavierShelton <121927710+AntonyXavierShelton@users.noreply.github.com>
2023-02-27 16:07:49 +0530

    Leave module : Leave apply file Created in Vue

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -1,8 +1,18 @@
-<html>
-	<head>
-		@vite('resources/js/app.js')
-	</head>
-	<body>
-	<div id="app"></div>
-	</body>
-</html>
+@extends('layouts.master')
+@section('title')
+    @lang('translation.dashboards')
+@endsection
+@section('css')
+    <link rel="stylesheet" href="{{ URL::asset('/assets/css/calendar-vanila.css') }}"> 
+    <script src="{{ URL::asset('assets/js/calendar-vanila.js') }}" defer></script>
+       <link href="{{ URL::asset('assets/css/attendance.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('/assets/css/pages_profile.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" /> 
+@endsection
+@section('content')
+@vite('resources/js/hrms/modules/leave_module/leave_apply/LeaveApply.js')
+
+
+<div id="vjs_leaveApply"></div>
+
+@endsection
