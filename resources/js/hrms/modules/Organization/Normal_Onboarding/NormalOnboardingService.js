9fbd65ae718d68446823b31cf5419edd00d904c7
AntonyXavierShelton <121927710+AntonyXavierShelton@users.noreply.github.com>
2023-03-03 15:12:40 +0530

    NormalOnboarding minor changes in Variables

--- resources/js/hrms/modules/Organization/Normal_Onboarding/NormalOnboardingService.js
+++ resources/js/hrms/modules/Organization/Normal_Onboarding/NormalOnboardingService.js
@@ -1,5 +1,5 @@
 /*
-Normal Onboarding 
+Normal Onboarding
 
 HTML fields
 Validation
@@ -118,22 +118,22 @@ export default function validation(rules) {
         //  Person Details End
 
         // Address Validation Start
-        currentAddress1: {
+        CurrentAddress1: {
             required,
         },
-        currentAddress2: {
+        CurrentAddress2: {
             required,
         },
-        currentcountry: {
+        CurrentCountry: {
             required,
         },
-        currentstate: {
+        CurrentState: {
             required,
         },
-        currentCity: {
+        CurrentCity: {
             required,
         },
-        currentPincode: {
+        CurrentPincode: {
             required,
             maxLength: 6,
         },
