e774989514ea08bd24b49242c94195e8e5240c1b
Karthi <karthigaiselvan28062000@gmail.com>
2023-03-06 12:42:11 +0530

    WIP: Attendance Settings Controller

---
+++ app/Http/Controllers/VmtAttendanceSettingsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\VmtEmployeeWorkShifts;
+
+class VmtAttendanceSettingsController extends Controller
+{
+    //
+
+    public function showAttendanceSettingsPage(Request $request){
+
+    }
+
+    public function assignEmployeesWorkShift(Request $request){
+
+        //VmtEmployeeWorkShifts
+    }
+}
