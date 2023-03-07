3068171d4a7a734c9c0ce6726b9da4f13962a48b
Praveen <praveenmax@users.noreply.github.com>
2023-03-08 00:00:43 +0530

    Reimbursements : Added base code for controller.

---
+++ app/Http/Controllers/VmtReimbursementController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VmtReimbursementController extends Controller
+{
+    public function showReimbursementsPage(Request $request){
+       // return view('')
+    }
+
+    public function fetchReimbursementsData(Request $request){
+
+    }
+
+    public function saveReimbursementsData(Request $request){
+
+    }
+}
