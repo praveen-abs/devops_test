7ce66170fbed5f0354fa932dcaac25f09f2853fb
Praveen <praveenmax@users.noreply.github.com>
2023-03-10 12:41:34 +0530

    Minor bug fix of VJS js paths

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
                     // Leaves
                     'resources/js/hrms/modules/leave_module/leave_apply/LeaveApply.js',
                     'resources/js/hrms/modules/leave_module/org_leave_module/OrgLeaveBalance.js',
+                    'resources/js/hrms/modules/leave_module/org_leave_module/OrgLeaveHistoryTable.js',
 
                     'resources/js/hrms/modules/leave_module/team_leave_module/TeamLeaveBalance.js',
                     //Reports
