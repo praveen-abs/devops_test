b2c75b8ac0ebb6cb098dd6ebd0fc2f2736fdaa93
Narasimma <sathishrain2001@gmail.com>
2023-03-21 12:48:55 +0530

    leave apply ui alignment fix

--- resources/js/hrms/modules/leave_module/leave_apply/LeaveApply.vue
+++ resources/js/hrms/modules/leave_module/leave_apply/LeaveApply.vue
@@ -314,13 +314,13 @@
                     </div>
                     <div class="col-md-12  col-sm-12 col-lg-8 col-xl-6 col-xxl-6  mb-md-0 mb-3">
                         <div class="form-group">
-                            <Textarea :autoResize="true" rows="3" cols="60" placeholder="Enter the Reason"
+                            <Textarea :autoResize="true" rows="3" cols="90" placeholder="Enter the Reason"
                                 v-model="service.leave_data.leave_reason" class="form-control" />
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-5 col-lg-4 col-sm-12 ">
+            <div class="col-md-5 col-lg-5 col-sm-12 ">
                 <div class="col-lg-12  ">
                     <Calendar :inline="true" :showWeek="true" style="min-width:100%" />
                 </div>
@@ -457,7 +457,6 @@ label {
 
 .p-chips .p-chips-multiple-container {
     padding: 0.375rem 0.75rem;
-    min-width: 315px;
 }
 
 .p-chips.p-component.p-inputwrapper {
