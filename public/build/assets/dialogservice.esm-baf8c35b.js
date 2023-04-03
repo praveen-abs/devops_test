4ae3ba4c34fa07d353d34c2ff1ff9fec85a23fd2
Praveen <praveenmax@users.noreply.github.com>
2023-04-03 17:24:01 +0530

    --- VJS BUILD ---

--- public/build/assets/dialogservice.esm-baf8c35b.js
+++
@@ -1,1 +0,0 @@
-import{A as l,V as s}from"./toastservice.esm-00869179.js";const c=Symbol();var t=l(),g={install:e=>{const i={open:(r,a)=>{const o={content:r&&s(r),options:a||{},data:a&&a.data,close:n=>{t.emit("close",{instance:o,params:n})}};return t.emit("open",{instance:o}),o}};e.config.unwrapInjectedRef=!0,e.config.globalProperties.$dialog=i,e.provide(c,i)}};export{g as D,t as a};
