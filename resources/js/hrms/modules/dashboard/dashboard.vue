e37b3c18cad6b230c5991ec1a1179868414fd835
Pradeesh <pradeesharumugam96@gmail.com>
2023-04-04 11:14:07 +0530

    Event is completed

--- resources/js/hrms/modules/dashboard/dashboard.vue
+++ resources/js/hrms/modules/dashboard/dashboard.vue
@@ -34,7 +34,7 @@
             <div class="col-sm-12 col-md-12 col-xl-12 col-lg-12 col-xxl-12">
                 <div class="row">
                     <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 ipad-query">
-                     <!-- <Events /> -->
+                     <Events />
 
                     </div>
                 </div>
