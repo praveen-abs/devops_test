5a9afd51f7d49473150760cf025f08e905b1497b
Pradeesh <pradeesharumugam96@gmail.com>
2023-04-17 14:56:42 +0530

    WIP : salary_advance.vue

--- resources/js/hrms/modules/salary_loan_setting/salary_advance/salary_advance.vue
+++ resources/js/hrms/modules/salary_loan_setting/salary_advance/salary_advance.vue
@@ -1,18 +1,39 @@
 <template>
     <div>
         <div class="">
-            <h1>Salary Advance Feature</h1>
-            <div class="div">
-                <button >Disabled</button>
+            <h1 class="fs-4 ">Salary Advance Feature</h1>
+            <div class="div ">
+                <button @click="Enabled_Hidden_div()">Disabled</button>
                 <button>Enabled</button>
             </div>
         </div>
-        <div class="div">
+        <div class="div" v-bind="Enabled" >
            <h1> Please click the "Enable" button to activate the salary advance feature for use within your organization.</h1>
         </div>
-        <div> <button style="color: var(--orange);">style</button> </div>
+        <div><button style="color: var(--orange);" >style</button> </div>
     </div>
 </template>
+
+<script setup>
+
+// import { ref } from "vue";
+
+
+// const Enabled  = ref(false);
+
+// const Disabled = ref(false);
+
+// function Enabled_Hidden_div(){
+// if(Enabled.value === false){
+
+
+
+// }
+// }
+
+
+</script>
+
 
 <style>
 :root{
@@ -22,5 +43,16 @@
     --darkgrey:#494949;
 
 }
+button{
+    padding: 8px 12px ;
+    margin: 0 10px;
+    border: 1px solid ;
+}
+.justify-center{
+    display: flex;
+    justify-content: start;
+    align-content: center;
+}
+
 
 </style>
