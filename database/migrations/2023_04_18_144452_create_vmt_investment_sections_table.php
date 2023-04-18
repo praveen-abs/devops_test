daa8e4920dd19f863085ba77545e7b62685954df
Praveen <praveenmax@users.noreply.github.com>
2023-04-18 18:16:12 +0530

    Migration : Investments : Wrote the model and migrations files.
    Need to populate sample data

---
+++ database/migrations/2023_04_18_144452_create_vmt_investment_sections_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vmt_investment_sections', function (Blueprint $table) {
+            $table->id();
+            $table->text('section_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vmt_investment_sections');
+    }
+};
