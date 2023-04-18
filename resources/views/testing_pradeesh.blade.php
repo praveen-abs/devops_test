8f187340687f62b2492921b90b6a0f305e620b3c
Praveen <praveenmax@users.noreply.github.com>
2023-04-18 17:28:09 +0530

    VJS : Employee Docs upload blade converted to VUE.
    Todo : Need to write axios

---
+++ resources/views/testing_pradeesh.blade.php
@@ -0,0 +1,37 @@
+<?php
+
+    use Illuminate\Support\Facades\Storage;
+?>
+
+@extends('layouts.master')
+@section('title')
+    @lang('translation.dashboards')
+@section('css')
+<link rel="stylesheet" href="{{ URL::asset('/assets/premassets/css/onboarding.css') }}">
+@endsection
+@endsection
+@section('content')
+<!doctype html>
+<html lang="en">
+<head>
+
+
+</head>
+<body>
+
+
+  @vite('resources/js/hrms/modules/Organization/employee_docs_upload/EmployeeDocsUpload.js')
+   <div id="EmployeeDocsUpload"></div>
+
+
+    {{-- <!-- @vite('resources/js/hrms/modules/paycheck/investments/investment.js')
+   <div id="Investments"></div> -->
+
+   @vite('resources/js/hrms/modules/salary_loan_setting/salary_loan_setting.js')
+   <div id="SalaryAdvanceLoan"></div> --}}
+
+
+</body>
+</html>
+@endsection
+
