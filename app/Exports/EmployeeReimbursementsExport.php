10606b6963ca95409d827290e9846e171469008a
Karthi <karthigaiselvan28062000@gmail.com>
2023-04-18 12:24:00 +0530

    WIP: Employee Reimbursements Report

--- app/Exports/EmployeeReimbursementsExport.php
+++ app/Exports/EmployeeReimbursementsExport.php
@@ -3,9 +3,26 @@
 namespace App\Exports;
 
 use App\Models\VmtEmployeeReimbursements;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Sheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\Style;
+use PhpOffice\PhpSpreadsheet\Style\Color;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use PhpOffice\PhpSpreadsheet\Style\Protection;
 
-class EmployeeReimbursementsExport implements FromCollection
+class EmployeeReimbursementsExport implements FromArray,ShouldAutoSize,WithHeadings,WithCustomStartCell,WithStyles,WithEvents,WithDrawings
 {
     /**
     * @return \Illuminate\Support\Collection
