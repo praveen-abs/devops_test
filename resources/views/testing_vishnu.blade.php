5984090351648d24bc4e032f8470cfcca3134f02
Vishnu <vvishva185@gmail.com>
2023-04-19 17:43:41 +0530

    Minor Fix:: bulkonboard doc uploads

--- resources/views/testing_vishnu.blade.php
+++ resources/views/testing_vishnu.blade.php
@@ -1,6 +1,8 @@
 <?php
 
     use Illuminate\Support\Facades\Storage;
+    use App\Models\VmtOnboardingDocuments;
+    use App\Models\VmtEmployeeDocuments;
 ?>
 
 @extends('layouts.master')
@@ -19,12 +21,41 @@
 </head>
 <body>
 <?php
+// $onboard_document_type = 'Aadhar sCard Front';
+// $onboard_doc_id = VmtOnboardingDocuments::where('document_name',$onboard_document_type)->first();
 
-$doc_filename='AadharCardFront_B090_17-04-2023_12-22-55.jpg';
-$private_file = "B090/onboarding_documents/DrivingLicense_B090_17-04-2023_12-22-55.jpg";
-    //dd(file(storage_path('employees/'.$private_file)));
-    $response= response()->file(storage_path('employees/'.$private_file));
-   return  base64_encode( $response);
+// if($onboard_doc_id == null)
+//     echo("doc empty");
+
+//dd($onboard_doc_id);
+    $user_docs = VmtOnboardingDocuments::leftJoin('vmt_employee_documents', 'vmt_employee_documents.doc_id', '=', 'vmt_onboarding_documents.id')
+                                        ->where('vmt_employee_documents.user_id',auth()->user()->id)
+                                        ->where('vmt_onboarding_documents.is_mandatory','1')
+                                        ->get(['vmt_employee_documents.user_id','vmt_onboarding_documents.document_name','vmt_employee_documents.doc_url']);
+
+    //dd($user_docs->toArray());
+    $mandatory_doc_ids = VmtOnboardingDocuments::where('is_mandatory','1')->pluck('id');
+
+    $user_uploaded_docs_ids = VmtEmployeeDocuments::whereIn('doc_id',$mandatory_doc_ids)
+                    ->where('vmt_employee_documents.user_id',auth()->user()->id)
+                    ->pluck('doc_id');
+
+    echo("Mandatory Docs IDS : ".$mandatory_doc_ids);
+    echo("<br/>User Uploaded Docs IDS : ".$user_uploaded_docs_ids);
+
+
+    $missing_mandatory_doc_ids = array_diff($mandatory_doc_ids->toArray(), $user_uploaded_docs_ids->toArray());
+     //dd($missing_mandatory_doc_ids);
+     foreach($missing_mandatory_doc_ids as $single_mandatory_id){
+       // $missing_mandatory_doc_name[] = VmtOnboardingDocuments::where('id',$single_mandatory_id)->first()->document_name;
+     }
+     //dd($missing_mandatory_doc_name);
+
+    if(count($mandatory_doc_ids) == count($user_uploaded_docs_ids)){
+              dd('hiii');
+    }else{
+        echo '<br/>bye';
+    }
 ?>
 
 </body>
