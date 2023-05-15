835d9c5d9238a62fcb5ebcaedf58bcbc53a664cf
Narasimma <sathishrain2001@gmail.com>
2023-05-15 16:19:08 +0530

    WIP investment

--- app/Models/VmtInvEmpFormdata.php
+++ app/Models/VmtInvEmpFormdata.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class VmtInvEmpFormdata extends Model
 {
@@ -11,4 +12,10 @@ class VmtInvEmpFormdata extends Model
 
     protected $table = "vmt_inv_emp_formdata";
 
-}
+    protected $fillable = [ 'f_emp_id ','fs_id','dec_amount','json_popups_value'];
+
+
+
+
+    }
+
