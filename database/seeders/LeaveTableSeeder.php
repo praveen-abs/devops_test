935fc087767c4f1ced1691808cf8f698d943530f
Narasimma <sathishrain2001@gmail.com>
2023-05-30 12:22:03 +0530

    add id in all seeder file

--- database/seeders/LeaveTableSeeder.php
+++ database/seeders/LeaveTableSeeder.php
@@ -19,13 +19,13 @@ class LeaveTableSeeder extends Seeder
         DB::table('vmt_leaves')->truncate();
 
         DB::table('vmt_leaves')->insert([
-            ['leave_type'=>'Sick Leave/Casual Leave','days_annual'=>'12','days_monthly'=>'2','days_restricted'=>'3','is_finite'=>'1'],
-            ['leave_type'=>'Earned Leave','days_annual'=>'12','days_monthly'=>'2','days_restricted'=>'2','is_finite'=>'1'],
-            ['leave_type'=>'Maternity Leave','days_annual'=>'84','days_monthly'=>'3','days_restricted'=>'0','is_finite'=>'1'],
-            ['leave_type'=>'Paternity Leave','days_annual'=>'15','days_monthly'=>'3','days_restricted'=>'1','is_finite'=>'1'],
-            ['leave_type'=>'On Duty','days_annual'=>'15','days_monthly'=>'3','days_restricted'=>'1','is_finite'=>'1'],
-            ['leave_type'=>'Permission','days_annual'=>'-1','days_monthly'=>'-1','days_restricted'=>'-1','is_finite'=>'1'],
-            ['leave_type'=>'Compensatory Off','days_annual'=>'0','days_monthly'=>'0','days_restricted'=>'0','is_finite'=>'0'],
+            ['id'=>'1','leave_type'=>'Sick Leave/Casual Leave','days_annual'=>'12','days_monthly'=>'2','days_restricted'=>'3','is_finite'=>'1'],
+            ['id'=>'2','leave_type'=>'Earned Leave','days_annual'=>'12','days_monthly'=>'2','days_restricted'=>'2','is_finite'=>'1'],
+            ['id'=>'3','leave_type'=>'Maternity Leave','days_annual'=>'84','days_monthly'=>'3','days_restricted'=>'0','is_finite'=>'1'],
+            ['id'=>'4','leave_type'=>'Paternity Leave','days_annual'=>'15','days_monthly'=>'3','days_restricted'=>'1','is_finite'=>'1'],
+            ['id'=>'5','leave_type'=>'On Duty','days_annual'=>'15','days_monthly'=>'3','days_restricted'=>'1','is_finite'=>'1'],
+            ['id'=>'6','leave_type'=>'Permission','days_annual'=>'-1','days_monthly'=>'-1','days_restricted'=>'-1','is_finite'=>'1'],
+            ['id'=>'7','leave_type'=>'Compensatory Off','days_annual'=>'0','days_monthly'=>'0','days_restricted'=>'0','is_finite'=>'0'],
 
         ]);
     }
