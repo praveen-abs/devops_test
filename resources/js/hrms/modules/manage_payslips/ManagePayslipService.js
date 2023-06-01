0cf4adc22727575ae9a543ae228332676a3f36d2
Vishnu <vvishva185@gmail.com>
2023-06-01 19:30:17 +0530

    minor change ManagePayslipService

--- resources/js/hrms/modules/manage_payslips/ManagePayslipService.js
+++ resources/js/hrms/modules/manage_payslips/ManagePayslipService.js
@@ -1,8 +1,11 @@
 import { defineStore } from "pinia";
 import { ref, reactive } from "vue";
 import axios from "axios";
+import { inject } from "vue";
 
 export const useManagePayslipStore = defineStore("managePayslipStore", () => {
+
+    const swal = inject("$swal");
 
 
     // Variable Declarations
@@ -81,7 +84,7 @@ export const useManagePayslipStore = defineStore("managePayslipStore", () => {
             console.log("testing ",response.data);
             if (response.data.status == "success"){
                 console.log(response.data.status);
-                Swal.fire({
+                swal.fire({
                        title: response.data.status = "success",
                        text: response.data.message,
                        icon: "success",
