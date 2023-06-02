4895d510d260cc96e8f6e0bb489898db007a80e2
Pradeesh <pradeesharumugam96@gmail.com>
2023-06-02 15:15:53 +0530

    minor change :: attendance_timesheet

--- resources/views/attendance_timesheet.blade.php
+++ resources/views/attendance_timesheet.blade.php
@@ -1752,7 +1752,7 @@ $svg_icon_notApplied = '/images/icons/svg_icon_notApplied.svg';
                                         }
 
 
-                                        cell.innerHTML = " <div class='p-2 w-100 h-100'><p class='show_date' >" + date +
+                                        cell.innerHTML = " <div class='p-2 w-100 h-100'><p class='show_date w-full d-flex align-items-center justify-content-between' >" + date + "<span style='font-size:9px;' class=' bg-green-300 px-1 rounded'>MOP</span>"+
 
                                         "</p>  <div class='mt-2 d-flex flex-column bio_check align-items-center' ><div class='check-in f-10 text-success w-100 d-flex align-items-center justify-content-start'><i class='fa fa-arrow-down me-1' style='transform: rotate(-45deg);'></i><span class='f-11' id='checkin_time_" +
                                         year + "-" + processedMonth + "-" + dateText + "'>" + ui_final_checkin_time +
