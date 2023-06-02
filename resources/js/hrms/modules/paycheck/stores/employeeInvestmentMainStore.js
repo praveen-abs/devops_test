0620365355e74054032af39b083cbac97f60108e
AntonyXavierShelton <121927710+AntonyXavierShelton@users.noreply.github.com>
2023-06-01 22:22:45 -0700

    Investment WIP

---
+++ resources/js/hrms/modules/paycheck/stores/employeeInvestmentMainStore.js
@@ -0,0 +1,18 @@
+import {defineStore } from "pinia";
+import {ref, reactive} from "vue";
+import axios from "axios";
+
+
+
+
+export const employeeInvestmentMainStore = defineStore("employeeInvestmentMainStore", () => {
+
+
+
+
+  return{
+    
+  }
+
+
+});
