c6a633f206287433363a401b748dfbf426878385
AntonyXavierShelton <121927710+AntonyXavierShelton@users.noreply.github.com>
2023-06-08 23:40:25 -0700

    Payslip Template Created

---
+++ resources/js/hrms/modules/payroll/payslip/templates/template1.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="w-full  card">
+        <div class="card-body p-6">
+            <div class="flex justify-between ">
+                <div>
+                    <p class="text-xl "><strong class="font-semibold fs-2 text-black">PAYSLIP</strong><strong
+                            class="font-semibold fs-2 mx-2 text-gray-600">MAR 2023</strong></p>
+                    <p class="my-3 text-semibold fs-5">ARHA MEDIA & BROADCASTING PVT. LTD.</p>
+                    <p class="w-5">
+                        REGISTERED ADDRESS: 4TH FLOOR, 8-2-337,
+                        ROAD NO. 3, BANJARA HILLS,
+                        HYDERABAD TELANGANA 500034
+                    </p>
+                </div>
+                <div>
+                    <img src="../../../../assests/images/man(1).png" alt="">
+                </div>
+            </div>
+            <div class="my-4">
+                <div class="py-2">
+                    <strong class="font-semibold fs-5 text-black">
+                        SWATHI BALASUBRAMANIAN
+                    </strong>
+                </div>
+                <div>
+                    <div class="bg-black h-0.5 w-full"></div>
+                    <div class="grid gap-4 md:grid-cols-3 sm:grid-cols-1 xxl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-4">
+                        <div class="mx-2 py-3">
+                            <p class="text-gray-600 fs-4">xxxxxxx</p>
+                            <p class="font-semibold text-lg">1213</p>
+                            <!-- <div class="bg-gray-600 h-0.5 w-full my-2"></div> -->
+                        </div>
+                    </div>
+                    <div class="bg-black h-0.5 w-full"></div>
+                </div>
+            </div>
+            <div>
+                <div class="py-2">
+                    <strong class="font-semibold fs-5 text-black">
+                        Salary Details
+                    </strong>
+                </div>
+                <div>
+                    <div class="bg-black h-0.5 w-full"></div>
+                    <div class="grid gap-4 md:grid-cols-3 sm:grid-cols-1 xxl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-4">
+                        <div class="mx-2 py-3">
+                            <p class="text-gray-600 fs-4">xxxxxxx</p>
+                            <p class="font-semibold text-lg">1213</p>
+                            <!-- <div class="bg-gray-600 h-0.5 w-full my-2"></div> -->
+
+                        </div>
+                    </div>
+                    <div class="bg-black h-0.5 w-full"></div>
+                </div>
+            </div>
+            <div class="flex my-4">
+                <div class="w-6">
+                    <strong class="font-semibold fs-5 text-black font-bold py-4">
+                        EARNINGS
+                    </strong>
+                    <div class="flex justify-between gap-6 my-2">
+                        <div class="w-12">
+                            <p class="font-semibold text-lg">Salary Component</p>
+                        </div>
+                        <p class="font-semibold text-lg">100000</p>
+                    </div>
+                </div>
+                <div class="w-6">
+                    <strong class="font-semibold fs-5 text-black font-bold">
+                        CONTRIBUTIONS
+                    </strong>
+                    <div class="flex justify-between gap-6 my-2">
+                        <div class="w-12">
+                            <p class="font-semibold text-lg">Salary Component</p>
+                        </div>
+                        <p class="font-semibold text-lg">100000</p>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-black h-0.5 w-full"></div>
+
+        </div>
+    </div>
+</template>
--- resources/views/testing_shelly.blade.php
+++ resources/views/testing_shelly.blade.php
@@ -7,9 +7,10 @@
 @endsection
 @endsection
 @section('content')
-@vite('resources/js/hrms/modules/paycheck/investments/investment.js')
-<div id="Investments"></div>
+
 <!-- @vite('resources/js/hrms/modules/exit/exit.js')
 <div id="Exit"></div> -->
+@vite('resources/js/hrms/modules/payroll/payslip/payslipMaster.js')
+<div id="payslip"></div>
 </html>
 @endsection
