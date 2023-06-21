6f97b75d76d26b27133aa3c488a6e34d72efb807
Pradeesh <pradeesharumugam96@gmail.com>
2023-06-21 10:25:25 +0530

    WIP :

--- resources/js/hrms/modules/dashboard/employee_dashboard/holiday_widget/holiday_widget.vue
+++ resources/js/hrms/modules/dashboard/employee_dashboard/holiday_widget/holiday_widget.vue
@@ -1,6 +1,5 @@
 <template>
-        <div class="card">
-            <Carousel :value="holidays" :numVisible="1" :numScroll="1" :responsiveOptions="responsiveOptions">
+        <!-- <Carousel :value="holidays" :numVisible="1" :numScroll="1" :responsiveOptions="responsiveOptions">
                 <template #item="slotProps">
                     <div class="px-1 text-center h-90">
                         <img :src="`data:image/png;base64,${slotProps.data.image}`" class="mt-3 rounded shadow-sm" style="width: 290px; height: 135px; " :alt="slotProps.data.holiday_name" />
@@ -9,8 +8,15 @@
                         </div>
                     </div>
                 </template>
-            </Carousel>
-        </div>
+            </Carousel> -->
+            <Galleria :value="holidays" :responsiveOptions="responsiveOptions" :numVisible="5" :circular="true"
+            containerStyle="max-width: 500px" style="height: 100px;" :showItemNavigators="true" :showThumbnails="false">
+            <template #item="slotProps">
+                <img :src="`data:image/png;base64,${slotProps.item.image}`" class="mt-3 rounded shadow-sm" style="width:400px; height: 170px; display: block;"  :alt="slotProps.item.holiday_name" />
+            </template>
+        </Galleria>
+
+
 </template>
 <script setup>
 import axios from "axios";
@@ -51,7 +57,50 @@ onMounted(() => {
 </script>
 
 <style>
-.p-carousel-indicators.p-reset{
+.p-carousel-indicators.p-reset {
     display: none;
 }
 </style>
+
+
+{
+<!--
+<template>
+    <div class="card md:flex md:justify-content-center">
+        <Galleria :value="images" :responsiveOptions="responsiveOptions" :numVisible="5" :circular="true" containerStyle="max-width: 640px"
+            :showItemNavigators="true">
+            <template #item="slotProps">
+                <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="width: 100%; display: block;" />
+            </template>
+            <template #thumbnail="slotProps">
+                <img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" style="display: block;" />
+            </template>
+        </Galleria>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { PhotoService } from '@/service/PhotoService';
+
+onMounted(() => {
+    PhotoService.getImages().then((data) => (images.value = data));
+});
+
+const images = ref();
+const responsiveOptions = ref([
+    {
+        breakpoint: '991px',
+        numVisible: 4
+    },
+    {
+        breakpoint: '767px',
+        numVisible: 3
+    },
+    {
+        breakpoint: '575px',
+        numVisible: 1
+    }
+]);
+</script> -->
+}
