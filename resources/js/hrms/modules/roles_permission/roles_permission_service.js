e31ef8f028989326bd9998bd8d116fc97e259964
Pradeesh <pradeesharumugam96@gmail.com>
2023-06-23 11:15:49 +0530

    WIP ::  roles and permission

--- resources/js/hrms/modules/roles_permission/roles_permission_service.js
+++ resources/js/hrms/modules/roles_permission/roles_permission_service.js
@@ -7,27 +7,38 @@ export const UseRolePermissionServie = defineStore('RolePermissionServie',()=>{
 
     // Variable Declarations
     const array_RolePermission_data = ref();
+    const AllPermission = ref();
 
-const allpermission = ref();
+    const CreatingNewJobRole = reactive({
+        Role_Title:'',
+        Role_description:'',
+        Assign_to:'',
+    });
+
+
 
     const getAllPermissions = () => {
 
         axios.get('/getAllPermissions').then(res => {
-            allpermission.value = res.data;
+            AllPermission.value = res.data;
             console.log(allpermission);
         });
 
     };
+     const saveCreateNewJobRole = ()=>{
+        axios.post('',).finally(()=>{
 
+        })
+     };
 
     return{
 
         // variable Declaration
-        getAllPermissions,
+        getAllPermissions,saveCreateNewJobRole,
 
 
         // function
-        allpermission,
+        AllPermission,CreatingNewJobRole,array_RolePermission_data
 
 
     }
