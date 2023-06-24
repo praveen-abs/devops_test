685ac648f614d4e0716bd27468ae722e89b14fda
Pradeesh <pradeesharumugam96@gmail.com>
2023-06-24 17:52:39 +0530

    WIP ::

--- resources/js/hrms/modules/roles_permission/RolesPermission.vue
+++ resources/js/hrms/modules/roles_permission/RolesPermission.vue
@@ -16,32 +16,80 @@
             </div>
         </div>
     </div> -->
-    <div class="w-full">
-        <div>
-            <h4 class="px-4 text-2xl font-semibold ">Employee Roles and Permissions</h4>
+    <div class="w-full mt-30">
+        <div class="">
+            <h1 class="fs-2 fw-semibold my-3">User Roles</h1>
+            <p class=" fw-semibold mb-3">User Roles can be assigned to the employees from here. New roles can be created and
+                privileges for all these roles can be managed from this section.</p>
         </div>
-        <div class="p-4 my-4 card">
-            <div class="card-body">
-                <p class="text-lg font-semibold text-gray-700 fs-4">Here you can manage the Employees Roles and Permissions</p>
-                <div class="flex my-6">
-                    <InputText placeholder="Search...."  class="w-4 h-10"/>
-                    <!-- Creating New Job Roles dailog-->
-                    <button class="h-10 mx-6 btn btn-orange" @click="addNewroleDailog = true">Create Role</button>
+        <div class="card bg-blue-200 h-20 border-none p-4 ">
+            <div class="d-flex justify-content-between align-items-center ">
+                <div class=" w-80 ">
+                    <input type="text" name="" id="" placeholder="search" class="rounded h-10 w-80 pl-2 shadow-md">
                 </div>
-
-                <div>
-                    <DataTable>
-                        <Column field="product" header="Role"></Column>
-                        <Column field="lastYearSale" header="Who Has Access"></Column>
-                        <Column field="thisYearSale" header="Actions"></Column>
-                    </DataTable>
+                <div class="">
+                    <button class=" bg-blue-800 px-4 py-2 rounded text-white mx-3 shadow-md">save</button>
+                    <button class="bg-white text-blue-800 px-3 py-2 rounded shadow-md"> <i class="pi pi-plus"></i>
+                        New Role
+                    </button>
                 </div>
             </div>
+
         </div>
 
 
+        <div class="card shadow-md mt-4">
 
+
+            <DataTable :paginator="true" :rows="10" class="" dataKey="user_code" @rowExpand="onRowExpand"
+                @rowCollapse="onRowCollapse" v-model:expandedRows="expandedRows" v-model:selection="selectedAllEmployee"
+                :selectAll="selectAll" @select-all-change="onSelectAllChange" @row-select="onRowSelect"
+                @row-unselect="onRowUnselect" :rowsPerPageOptions="[5, 10, 25]"
+                paginatorTemplate="CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown"
+                responsiveLayout="scroll" currentPageReportTemplate="Showing {first} to {last} of {totalRecords}">
+                <template #empty> No Employee Details documents for the selected status filter </template>
+
+                <Column :expander="true" />
+                <Column selectionMode="multiple" style="width: 1rem" :exportable="false"></Column>
+                <Column field="roles" header="Roles" sortable></Column>
+                <Column field="name" header="Role Description">
+                </Column>
+
+                <Column field="doc_status" header="Assigned Privileges" :sortable="false">
+
+                </Column>
+                <Column field="doc_status" header="Assigned Employees" :sortable="false">
+
+                </Column>
+                <Column field="" header="Action">
+
+                </Column>
+                <template #expansion="slotProps">
+                    <div>
+                        <DataTable responsiveLayout="scroll" v-model:selection="selectedAllEmployee" :selectAll="selectAll"
+                            @select-all-change="onSelectAllChange">
+                            <Column field="user_code" header="Employee ID">{{ slotProps.data.doc_name }}</Column>
+                            <Column field="doc_status" header="Employee Name">
+
+                            </Column>
+                            <Column field="" header="Department">
+
+                            </Column>
+
+                            <Column field="" header="Action">
+
+                            </Column>
+                        </DataTable>
+                    </div>
+                </template>
+
+            </DataTable>
+
+        </div>
     </div>
+
+
+  
 
     <Dialog header="Header" v-model:visible="addNewroleDailog" :breakpoints="{ '960px': '75vw', '640px': '90vw' }"
         :style="{ width: '65vw', borderTop: '5px solid #002f56' }" :modal="true" :closable="false" :closeOnEscape="false">
@@ -66,11 +114,12 @@
             </div>
 
             <TreeTable :value="nodes" key="id">
-            <Column field="name" header="Name" expander>
-            <input type="checkbox"></Column>
-            <Column field="size" header="Size"></Column>
-            <Column field="type" header="Type"></Column>
-        </TreeTable>
+                <Column field="name" header="Name" expander>
+                    <input type="checkbox">
+                </Column>
+                <Column field="size" header="Size"></Column>
+                <Column field="type" header="Type"></Column>
+            </TreeTable>
         </div>
         <template #footer>
             <div>
@@ -81,7 +130,7 @@
 
         </template>
     </Dialog>
-     <!-- {{ selectedKey }} -->
+    <!-- {{ selectedKey }} -->
     <!-- {{ rolespermission.allpermission }} -->
     <!-- {{ allpermission.data }} -->
 </template>
@@ -92,7 +141,10 @@ import { onMounted, ref } from "vue";
 import { UseRolePermissionServie } from "./roles_permission_service";
 import axios from 'axios';
 
-const rolespermission = UseRolePermissionServie()
+const rolespermission = UseRolePermissionServie();
+
+const expandedRows = ref([]);
+const selectedAllEmployee = ref();
 
 const lding = ref(true)
 
@@ -106,53 +158,53 @@ const canShowManageRoles_Dialog = ref(false);
 const allpermission = ref();
 
 axios.get('/getAllPermissions').then(res => {
-            allpermission.value = res.data;
-            console.log(allpermission);
-        });
+    allpermission.value = res.data;
+    console.log(allpermission);
+});
 
 
-        const nodes =ref([
-                {
-                    id:1,
-                    key: '0',
-                    label: 'Documents',
-                    data: 'Documents Folder',
-                    icon: 'pi pi-fw pi-inbox',
-                    children: [
-                        {
+const nodes = ref([
+    {
+        id: 1,
+        key: '0',
+        label: 'Documents',
+        data: 'Documents Folder',
+        icon: 'pi pi-fw pi-inbox',
+        children: [
+            {
 
-                            id:2,
-                            key: '1',
-                            label: 'Work',
-                            data: 'Work Folder',
-                            icon: 'pi pi-fw pi-cog',
-                            children: [
-                                { key: '0-0-0', label: 'Expenses.doc', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
-                                { key: '0-0-1', label: 'Resume.doc', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
-                            ]
-                        },
-                        {
-                            id:3,
-                            key: '2',
-                            label: 'Home',
-                            data: 'Home Folder',
-                            icon: 'pi pi-fw pi-home',
-                            children: [{ key: '0-1-0', label: 'Invoices.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
-                        }
-                    ]
-                },
-                {
-                    id:4,
-                    key: '4',
-                    label: 'Events',
-                    data: 'Events Folder',
-                    icon: 'pi pi-fw pi-calendar',
-                    children: [
-                        {id:5, key: '1-0', label: 'Meeting', icon: 'pi pi-fw pi-calendar-plus', data: 'Meeting' },
-                        {id:6, key: '1-1', label: 'Product Launch', icon: 'pi pi-fw pi-calendar-plus', data: 'Product Launch' },
-                        {id:7, key: '1-2', label: 'Report Review', icon: 'pi pi-fw pi-calendar-plus', data: 'Report Review' }
-                    ]
-                }])
+                id: 2,
+                key: '1',
+                label: 'Work',
+                data: 'Work Folder',
+                icon: 'pi pi-fw pi-cog',
+                children: [
+                    { key: '0-0-0', label: 'Expenses.doc', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
+                    { key: '0-0-1', label: 'Resume.doc', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
+                ]
+            },
+            {
+                id: 3,
+                key: '2',
+                label: 'Home',
+                data: 'Home Folder',
+                icon: 'pi pi-fw pi-home',
+                children: [{ key: '0-1-0', label: 'Invoices.txt', icon: 'pi pi-fw pi-file', data: 'Invoices for this month' }]
+            }
+        ]
+    },
+    {
+        id: 4,
+        key: '4',
+        label: 'Events',
+        data: 'Events Folder',
+        icon: 'pi pi-fw pi-calendar',
+        children: [
+            { id: 5, key: '1-0', label: 'Meeting', icon: 'pi pi-fw pi-calendar-plus', data: 'Meeting' },
+            { id: 6, key: '1-1', label: 'Product Launch', icon: 'pi pi-fw pi-calendar-plus', data: 'Product Launch' },
+            { id: 7, key: '1-2', label: 'Report Review', icon: 'pi pi-fw pi-calendar-plus', data: 'Report Review' }
+        ]
+    }])
 
 onMounted(() => {
     setTimeout(() => {
