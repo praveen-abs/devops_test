9e2d90032403058ca9b019385d3cd338eca4c951
Pradeesh <pradeesharumugam96@gmail.com>
2023-07-03 18:18:21 +0530

    WIP ::

--- resources/js/hrms/modules/paycheck/salary_advance_loan/interest_free_loan/interest_free_loan.vue
+++ resources/js/hrms/modules/paycheck/salary_advance_loan/interest_free_loan/interest_free_loan.vue
@@ -4,32 +4,45 @@
             <div class="flex justify-between gap-6 my-2">
                 <div class=" fs-4">
                     <p class="text-xl font-medium">You are eligible for the Interest Free Loan as per the
-                        <span class="text-lg text-primary text-decoration-underline"> Company's Loan Policy</span></p>
+                        <span class="text-lg text-primary text-decoration-underline"> Company's Loan Policy</span>
+                    </p>
                 </div>
 
                 <div class="float-right">
                     <button class="btn btn-border-orange">View Report</button>
-                    <button class="mx-4 btn btn-orange" @click="openPosition('top')" >
+                    <button class="mx-4 btn btn-orange" @click="openPosition('top')">
                         <i class="mx-2 fa fa-plus" aria-hidden="true"></i>
                         New Request
                     </button>
 
-                    <Dialog v-model:visible="useEmpStore.dialog_NewInterestFreeLoanRequest" header="Header" :style="{ width: '58vw' }" modal :position="position" >
+                    <Dialog v-model:visible="useEmpStore.dialog_NewInterestFreeLoanRequest" header="Header"
+                        :style="{ width: '58vw' }" modal :position="position">
                         <template #header>
-                            <div><h1 style="border-left: 3px solid var( --orange);padding-left: 5px ;" class="fs-4">New Interest Free Loan Request</h1></div>
+                            <div>
+                                <h1 style="border-left: 3px solid var( --orange);padding-left: 5px ;" class="fs-4">New
+                                    Interest Free Loan Request</h1>
+                            </div>
                         </template>
                         <div class="card bg-gray-100 bottom-0 mb-10" style="border:none">
                             <div class="card-body">
                                 <div class="row mx-2">
                                     <div class="col mx-2">
                                         <h1 class="fs-5 my-2">Required Amount</h1>
-                                        <InputNumber  v-model.number="useEmpStore.ifl.Ra" placeholder="&#8377; Enter The Required Amount" inputId="withoutgrouping" :useGrouping="false" />
-                                        <!-- <InputText type="text"  v-model.number="useEmpStore.ifl.Ra" placeholder="&#8377; Enter The Required Amount" /> -->
-                                        <p class="fs-6 my-2" style="color: var(--clr-gray)">Max Eligible Amount : <span class=" fw-semibold">{{useEmpStore.ifl.minEligibile }}</span> </p>
+                                        <InputNumber v-model.number="useEmpStore.ifl.Ra"
+                                            placeholder="&#8377; Enter The Required Amount" inputId="withoutgrouping"
+                                            :useGrouping="false"
+                                            :class="[v$.Ra.$error ? ' border-2 outline-none border-red-500 rounded-lg' : '']" />
+                                        <br>
+                                        <span v-if="v$.Ra.$error" class="font-semibold text-red-400 fs-6">
+                                            {{ v$.Ra.$errors[0].$message }}
+                                        </span>
+                                        <p class="fs-6 my-2" style="color: var(--clr-gray)">Max Eligible Amount : <span
+                                                class=" fw-semibold">{{ useEmpStore.ifl.minEligibile }}</span> </p>
                                     </div>
                                     <div class="col mx-2">
                                         <h1 class="fs-5 my-2">Monthly EMI</h1>
-                                        <InputText type="text" readonly v-model="useEmpStore.ifl.M_EMI" placeholder="&#8377; " />
+                                        <InputText type="text" readonly v-model="useEmpStore.ifl.M_EMI"
+                                            placeholder="&#8377; " />
                                     </div>
                                     <div class="col mx-2">
                                         <h1 class="fs-5 my-2">Term</h1>
@@ -37,8 +50,16 @@
 
                                         <!-- <Dropdown v-model="useEmpStore.ifl.Term" @change="selectMonth" :options="useEmpStore.ifl.max_tenure_months" optionLabel="month" placeholder="Select Month" class="w-full md:w-10rem" optionValue="val" /> -->
 
-                                        <Dropdown v-model="useEmpStore.ifl.Term" @change="selectMonth" :options="useEmpStore.ifl.max_tenure_months" class="w-full md:w-10rem"  optionValue="month" optionLabel="month" placeholder="Select Month" />
-                                        <label for="" class="fs-5 ml-2" >Month</label>
+                                        <Dropdown v-model="useEmpStore.ifl.Term" @change="selectMonth"
+                                            :options="useEmpStore.ifl.max_tenure_months" class="w-full md:w-10rem"
+                                            optionValue="month" optionLabel="month" placeholder="Select Month"
+                                            :class="[v$.Term.$error ? ' border-2 outline-none border-red-500 rounded-lg' : '']" />
+                                        <label for="" class="fs-5 ml-2">Month</label>
+                                        <br>
+                                        <span v-if="v$.Term.$error" class="font-semibold text-red-400 fs-6">
+                                            {{ v$.Term.$errors[0].$message }}
+                                        </span>
+
                                     </div>
                                 </div>
                             </div>
@@ -49,38 +70,54 @@
                                 <div class="row">
                                     <!-- fw-bolder -->
                                     <h1 class="fs-4 my-2  ">EMI Dedution</h1>
-                                    <h1 class="fs-5 text-gray-600 mb-3">The EMI Dedution Will begin from the Upcoming Payroll</h1>
-                                        <div class="col-4">
-                                            <h1 class="fs-5 my-2 ml-2">EMI Start Month</h1>
-                                            <!-- <Calendar v-model="useEmpStore.ifl.EMI_Start_Month" showIcon /> -->
-                                            <!-- {{useEmpStore.ifl.details.deduction_starting_month  }} -->
-                                            <Dropdown v-model="useEmpStore.ifl.EMI_Start_Month" @change="calculateMonth" :options="useEmpStore.ifl.details.deduction_starting_month" optionLabel="Month" placeholder="Select Month"  />
-                                        </div>
+                                    <h1 class="fs-5 text-gray-600 mb-3">The EMI Dedution Will begin from the Upcoming
+                                        Payroll</h1>
+                                    <div class="col-4">
+                                        <h1 class="fs-5 my-2 ml-2">EMI Start Month</h1>
+                                        <!-- <Calendar v-model="useEmpStore.ifl.EMI_Start_Month" showIcon /> -->
+                                        <!-- {{useEmpStore.ifl.details.deduction_starting_month  }} -->
+                                        <Dropdown v-model="useEmpStore.ifl.EMI_Start_Month" @change="calculateMonth"
+                                            :options="useEmpStore.ifl.details.deduction_starting_month" optionLabel="date"
+                                            optionValue="date" placeholder="Select Month"
+                                            :class="[v$.EMI_Start_Month.$error ? ' border-2 outline-none border-red-500 rounded-lg' : '']" />
+                                        <br>
+                                        <span v-if="v$.EMI_Start_Month.$error" class="font-semibold text-red-400 fs-6">
+                                            {{ v$.EMI_Start_Month.$errors[0].$message }}
+                                        </span>
+                                    </div>
 
-                                        <div class="col-4 mx-2">
-                                            <h1 class="fs-5 my-2 ml-2">EMI End Month</h1>
-                                            <!-- <InputText type="text" readonly v-model="useEmpStore.ifl.EMI_End_Month"  style="width: 150px !important;" /> -->
-                                            <Calendar v-model="useEmpStore.ifl.EMI_End_Month"  readonly  style="width: 150px !important;" />
-                                            <!-- showIcon -->
-                                        </div>
-                                        <div class="col-3">
-                                            <h1 class="fs-5 my-2 ml-2" >Total Months</h1>
-                                            <InputText type="text" readonly v-model="useEmpStore.ifl.Total_Months" style="width: 150px !important;" />
-                                        </div>
+                                    <div class="col-4 mx-2">
+                                        <h1 class="fs-5 my-2 ml-2">EMI End Month</h1>
+                                        <!-- <InputText type="text" readonly v-model="useEmpStore.ifl.EMI_End_Month"  style="width: 150px !important;" /> -->
+                                        <Calendar v-model="useEmpStore.ifl.EMI_End_Month" readonly
+                                            style="width: 150px !important;" />
+                                        <!-- showIcon -->
+                                    </div>
+                                    <div class="col-3">
+                                        <h1 class="fs-5 my-2 ml-2">Total Months</h1>
+                                        <InputText type="text" readonly v-model="useEmpStore.ifl.Total_Months"
+                                            style="width: 150px !important;" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
 
                         <div class="p-4 my-6 bg-gray-100 rounded-lg gap-6">
                             <span class="font-semibold ">Reason</span>
-                            <Textarea  class="my-3 capitalize form-control textbox" autoResize type="text" rows="3" v-model="useEmpStore.ifl.Reason" />
+                            <Textarea class="my-3 capitalize form-control textbox" autoResize type="text" rows="3"
+                                v-model="useEmpStore.ifl.Reason"
+                                :class="[v$.Reason.$error ? ' border-2 outline-none border-red-500 rounded-lg' : '']" />
+                            <br>
+                            <span v-if="v$.Reason.$error" class="font-semibold text-red-400 fs-6">
+                                {{ v$.Reason.$errors[0].$message }}
+                            </span>
                         </div>
-
 
                         <template #footer>
                             <div class="float-right ">
-                                <button class="btn btn-border-orange"  @click="dialog_NewInterestFreeLoanRequest = false">Cancel</button>
-                                <button  class="mx-4 btn btn-orange" @click="useEmpStore.saveInterestfreeLoan">Submit</button>
+                                <button class="btn btn-border-orange"
+                                    @click="dialog_NewInterestFreeLoanRequest = false">Cancel</button>
+                                <button class="mx-4 btn btn-orange" @click="submitForm">Submit</button>
                             </div>
                             <!-- <Button label="" icon="pi pi-times" @click="visible = false" text />
                             <Button label="Yes" icon="pi pi-check" @click="visible = false" text /> -->
@@ -163,33 +200,31 @@
 
 
 
-    <Dialog header="Header" v-model:visible="useEmpStore.canShowLoading" :breakpoints="{ '960px': '75vw', '640px': '90vw' }" :style="{ width: '25vw' }"
-    :modal="true" :closable="false" :closeOnEscape="false">
-    <template #header>
-      <ProgressSpinner style="width: 50px; height: 50px" strokeWidth="8" fill="var(--surface-ground)"
-        animationDuration="2s" aria-label="Custom ProgressSpinner" />
-    </template>
-    <template #footer>
-      <h5 style="text-align: center">Please wait...</h5>
-    </template>
-  </Dialog>
-
-
-
-
+    <Dialog header="Header" v-model:visible="useEmpStore.canShowLoading" :breakpoints="{ '960px': '75vw', '640px': '90vw' }"
+        :style="{ width: '25vw' }" :modal="true" :closable="false" :closeOnEscape="false">
+        <template #header>
+            <ProgressSpinner style="width: 50px; height: 50px" strokeWidth="8" fill="var(--surface-ground)"
+                animationDuration="2s" aria-label="Custom ProgressSpinner" />
+        </template>
+        <template #footer>
+            <h5 style="text-align: center">Please wait...</h5>
+        </template>
+    </Dialog>
 </template>
 
 <script setup>
-import { ref, reactive, onMounted } from 'vue';
-import {useEmpSalaryAdvanceStore}  from '../../stores/employeeSalaryAdvanceLoanMainStore';
+import { ref, reactive, onMounted, computed } from 'vue';
+import { useEmpSalaryAdvanceStore } from '../../stores/employeeSalaryAdvanceLoanMainStore';
 import dayjs from 'dayjs';
+import useValidate from '@vuelidate/core'
+import { required, email, minLength, sameAs, helpers } from '@vuelidate/validators'
 import { useNow, useDateFormat } from '@vueuse/core'
 
 const useEmpStore = useEmpSalaryAdvanceStore();
 
 onMounted(() => {
-   useEmpStore.fetchInterestfreeLoan()
-   useEmpStore.getinterestfreeloan();
+    useEmpStore.fetchInterestfreeLoan()
+    useEmpStore.getinterestfreeloan();
 })
 
 const select_date = ref();
@@ -203,63 +238,88 @@ const openPosition = (pos) => {
 }
 
 
-function selectMonth(){
+function selectMonth() {
 
-    useEmpStore.ifl.M_EMI =  useEmpStore.ifl.Ra/ useEmpStore.ifl.Term;
-    useEmpStore.ifl.Total_Months =   useEmpStore.ifl.Term;
+    useEmpStore.ifl.M_EMI = useEmpStore.ifl.Ra / useEmpStore.ifl.Term;
+    useEmpStore.ifl.Total_Months = useEmpStore.ifl.Term;
 
-    return calculateMonth();
+    //
+    if (useEmpStore.ifl.EMI_Start_Month) {
+        return calculateMonth();
+    }
     // useEmpStore.ifl.Ra
     // useEmpStore.ifl.Term
 }
 
-function calculateMonth(){
+function calculateMonth() {
 
-function addMonthsToDate(dateString, months) {
-  var dateParts = dateString.split('/'); // Split the string into an array of parts
-  var month = parseInt(dateParts[0]) - 1; // Subtract 1 to get the zero-based month value
-  var day = parseInt(dateParts[1]);
-  var year = parseInt(dateParts[2]);
+    function addMonthsToDate(dateString, months) {
+        console.log(dateString, months);
+        var dateParts = dayjs(dateString).format('MM/DD/YYYY').split('/'); // Split the string into an array of parts
+        var month = parseInt(dateParts[0]) - 1; // Subtract 1 to get the zero-based month value
+        var day = parseInt(dateParts[1]);
+        var year = parseInt(dateParts[2]);
+        console.log(dateParts);
 
-  var date = new Date(year, month, day); // Create a Date object from the parts
-  date.setMonth(date.getMonth() + months); // Add the specified number of months
+        var date = new Date(year, month, day); // Create a Date object from the parts
+        date.setMonth(date.getMonth() + months); // Add the specified number of months
 
-  // Format the resulting date back into a string in "MM/DD/YYYY" format
-  var formattedDate = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
-  return formattedDate;
-}
+        // Format the resulting date back into a string in "MM/DD/YYYY" format
+        var formattedDate = date.getFullYear() + '-' + (date.getMonth()) + '-' + date.getDate();
+        return formattedDate;
+    }
 
-// Example usage
-var originalDate = useEmpStore.ifl.EMI_Start_Month.Month;
-var modifiedDate = addMonthsToDate(originalDate, useEmpStore.ifl.Term);
+    // Example usage
+    var originalDate = useEmpStore.ifl.EMI_Start_Month.Month;
+    var modifiedDate = addMonthsToDate(dayjs(originalDate).format('MM/DD/YYYY'), useEmpStore.ifl.Term);
 
-console.log(modifiedDate);
+    console.log(modifiedDate);
 
-useEmpStore.ifl.EMI_End_Month = modifiedDate;
+    useEmpStore.ifl.EMI_End_Month = dayjs(modifiedDate).format('YYYY-MM-DD');
 
-
-
-//  let values = dayjs(useEmpStore.ifl.EMI_Start_Month.Month).add(1,'month').format('YYYY/MM/DD');
-//  console.log(values);
+    //  let values = dayjs(useEmpStore.ifl.EMI_Start_Month.Month).add(1,'month').format('YYYY/MM/DD');
+    //  console.log(values);
 
 }
 
 const value = ref();
 
-if(useEmpStore.ifl.Term){
-console.log("testing ::",useEmpStore.ifl.Term);
+if (useEmpStore.ifl.Term) {
+    console.log("testing ::", useEmpStore.ifl.Term);
 }
 
 const selectedCity = ref();
-    const cities = ref([
-        { name: 1, val:2 },
-        { name: '2', code: 'RM' },
-        { name: '2.5', code: 'LDN' },
-        { name: '3', code: 'IST' },
-        { name: '3.5', code: 'PRS' }
-    ]);
+const cities = ref([
+    { name: 1, val: 2 },
+    { name: '2', code: 'RM' },
+    { name: '2.5', code: 'LDN' },
+    { name: '3', code: 'IST' },
+    { name: '3.5', code: 'PRS' }
+]);
 
-    const date = ref();
+const date = ref();
+
+
+const eligibleRequiredAmount = (value) => {
+    if (value > useEmpStore.ifl.minEligibile) {
+        return false
+    } else {
+        return true
+    }
+}
+
+
+const rules = computed(() => {
+    return {
+        Ra: { required: helpers.withMessage('Required amount is required', required), eligibleRequiredAmount: helpers.withMessage('Must be lesser than max eligible amount', eligibleRequiredAmount) },
+        Term: { required },
+        EMI_Start_Month: { required },
+        Reason: { required },
+    }
+})
+
+
+const v$ = useValidate(rules, useEmpStore.ifl)
 
 
 const submitForm = () => {
@@ -267,7 +327,7 @@ const submitForm = () => {
     if (!v$.value.$error) {
         // if ANY fail validation
         console.log('Form successfully submitted.')
-        useEmpStore.saveSalaryAdvance()
+        useEmpStore.saveInterestfreeLoan()
         v$.value.$reset()
     } else {
         console.log('Form failed validation')
@@ -281,11 +341,14 @@ const submitForm = () => {
     --orange: #FF4D00;
     --white: #fff;
     --navy: #002f56;
-    --clr-gray:#959595;
+    --clr-gray: #959595;
 }
- .pi-calendar , .p-button-label{
+
+.pi-calendar,
+.p-button-label {
     color: white !important;
 }
+
 .orange_btn {
     background-color: var(--orange);
     padding: 7px 30px;
@@ -306,15 +369,15 @@ const submitForm = () => {
     border-radius: 4px 0 0 4px;
 
 }
+
 .p-button.p-component.p-button-icon-only.p-datepicker-trigger {
     /* background: #002f56; */
     color: #002f56;
 }
-.p-calendar .p-component .p-inputwrapper .p-calendar-w-btn{
-margin: 0;
+
+.p-calendar .p-component .p-inputwrapper .p-calendar-w-btn {
+    margin: 0;
 }
-
-
 </style>
 
 {
