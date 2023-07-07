cb2b56525bb47ebb2ed6fd8a43aa01788edfc3cd
Narasimma <sathishrain2001@gmail.com>
2023-07-07 14:55:42 +0530

    salary_adv WIP

---
+++ database/migrations/2023_07_07_123823_add_column_vmt_salary_adv_setting_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vmt_salary_adv_setting', function (Blueprint $table) {
+            $table->text('can_borrowed_multiple')->after('approver_flow');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        dropColumnIfExists("vmt_salary_adv_setting",'can_borrowed_multiple');
+
+    }
+};
