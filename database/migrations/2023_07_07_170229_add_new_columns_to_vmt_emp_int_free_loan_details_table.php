4c866a59c93fbf1dc5a433f0423912cb31a1216c
Karthi <karthigaiselvan@abshrms.com>
2023-07-07 17:33:12 +0530

    Migration: Loan And Advance

---
+++ database/migrations/2023_07_07_170229_add_new_columns_to_vmt_emp_int_free_loan_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vmt_emp_int_free_loan_details', function (Blueprint $table) {
+            $table->text('loan_status')->after('loan_crd_sts');
+            $table->text('reviewer_cmds')->after('loan_status');
+            $table->date('paid_date')->after('reviewer_cmds');
+            $table->text('UTR_number')->after('paid_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vmt_emp_int_free_loan_details', function (Blueprint $table) {
+            //
+        });
+    }
+};
