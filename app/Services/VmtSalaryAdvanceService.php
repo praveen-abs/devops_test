0a000e27e9bb3e05ee86201432ef44d6d54bd608
Narasimma <sathishrain2001@gmail.com>
2023-07-14 23:46:13 +0530

    salary_adv minor change

--- app/Services/VmtSalaryAdvanceService.php
+++ app/Services/VmtSalaryAdvanceService.php
@@ -1369,6 +1369,7 @@ class VmtSalaryAdvanceService
                     $emp_details->request_id,
                     $result,
                     request()->getSchemeAndHttpHost(),
+                    $emp_details->sal_adv_status
                 ));
 
 
