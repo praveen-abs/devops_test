998b4bbaacbbd0eb0678612989861f8f515e3019
Pradeesh <pradeesharumugam96@gmail.com>
2023-07-17 13:38:11 +0530

    minor change ::

--- resources/js/hrms/modules/profile_pages/EmployeeCard/EmployeeCard.vue
+++ resources/js/hrms/modules/profile_pages/EmployeeCard/EmployeeCard.vue
@@ -294,12 +294,12 @@
 
 
             <div class="card p-3 d-flex  justify-items-center align-items-center"
-            style="width: 20rem; height: 340px; flex-direction: column !important;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
+            style="width: 20rem; height: 380px; flex-direction: column !important;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
 
-            <div style="height: 45px;width:140px;">
+            <div style="height: 45px;width:140px;" class=" mt-2">
                 <img :src="`${_instance_profilePagesStore.employeeDetails.client_logo}`" alt="" style=" object-fit: cover; " >
             </div>
-            <div class="card-body d-flex justify-items-center align-items-center mt-4" style="flex-direction: column ; ">
+            <div class="card-body d-flex justify-items-center align-items-center mt-6" style="flex-direction: column ; ">
                 <div class="mx-auto rounded-circle img-xl userActive-status profile-img "
                     style="border: 1px solid navy;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
                     <img v-if="_instance_profilePagesStore.profile"
@@ -312,12 +312,12 @@
                 <!-- <img v-if="profile" class="rounded-circle   profile-img"
                     :src="`data:image/png;base64,${profile}`" srcset=""  /> -->
 
-                <h1 class="card-title mt-5 mb-2 f-12 text-blue-900 subpixel-antialiased font-semibold" style="text-align: center;"> {{
+                <h1 class="card-title mt-12 mb-3 f-12 text-blue-900 subpixel-antialiased font-semibold" style="text-align: center;"> {{
                     _instance_profilePagesStore.employeeDetails.name }}</h1>
 
                 <h5 v-if="_instance_profilePagesStore.employeeDetails
                     .get_employee_office_details.department_id
-                    " class="f-12  card-text mb-2 text-gray-600 subpixel-antialiased font-semibold">
+                    " class="f-12  card-text mb-3 text-gray-600 subpixel-antialiased font-semibold">
                     {{
                         _instance_profilePagesStore.employeeDetails
                             .get_employee_office_details.department_name
@@ -336,42 +336,41 @@
             <!-- Digit-al Id back side  -->
 
             <div class="card p-2 d-flex justify-items-center align-items-center"
-            style="width: 20rem; height: 340px; flex-direction: column !important;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
+            style="width: 21rem; height: 380px; flex-direction: column !important;box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;">
             <div class=" w-100 d-flex justify-content-center align-items-center flex-column">
-                <h1 class=" text-orange-500 fs-14 subpixel-antialiased  fw-600">EMPLOYEE DETAILS</h1>
+                <h1 class=" text-orange-500 fs-14 subpixel-antialiased font-semibold  fw-600">EMPLOYEE DETAILS</h1>
                 <div class="row  w-100 mt-2">
                     <div class="col-6 fs-14 subpixel-antialiased  font-semibold text-left text-blue-900">
                         Blood Group :
                     </div>
-                    <div class="col-5 subpixel-antialiased font-semibold fs-6">
-                     {{ blood_group_id }}
+                    <div class="col-5 subpixel-antialiased font-semibold fs-6 text-blue-900">
+                     {{ cmpBldGrp }}
                     </div>
                 </div>
                 <div class="row  w-100">
                     <div class="col-6 fs-14 subpixel-antialiased  font-semibold text-left text-blue-900">
                         Phone :
                     </div>
-                    <div class="col-5 subpixel-antialiased font-semibold fs-6">
-                     {{ blood_group_id }}
+                    <div class="col-5 subpixel-antialiased font-semibold fs-6 text-blue-900">
+                     {{ _instance_profilePagesStore.employeeDetails.get_employee_details.mobile_number }}
                     </div>
                 </div>
                 <div class="row w-100 ">
-                    <div class="col-6 fs-14 subpixel-antialiased  font-semibold text-left text-blue-900">
+                    <div class="col-5 fs-14 subpixel-antialiased  font-semibold text-left text-blue-900">
                        Email Id :
                     </div>
-                    <div class="col-5 subpixel-antialiased font-semibold fs-6">
-                     {{ blood_group_id }}
+                    <div class="col-6 subpixel-antialiased font-semibold fs-12">
+                        <h1 class=" text-center">{{ _instance_profilePagesStore.employeeDetails.get_employee_office_details.officical_mail }}</h1>
                     </div>
                 </div>
                 <div class="row  w-100 ">
                     <div class="col fs-14 subpixel-antialiased  font-semibold text-left text-blue-900">
-                        <h1 class=" text-orange-500 fs-12">Residential Address :</h1>
+                        <h1 class=" text-orange-500 fs-12 ">Residential Address :</h1>
                         <div class=" ml-2">
                             <p class=" text-blue-900 mt-2 subpixel-antialiased font-semibold fs-11">
                                 {{ _instance_profilePagesStore.employeeDetails.get_employee_details.current_address_line_1 }}
                             </p>
                         </div>
-
                     </div>
                 </div>
                 <div class="row bg-gradient-to-r from-violet-500 to-fuchsia-500 h-1 w-100 mt-2">
@@ -379,14 +378,13 @@
                 <div class="row bg-gradient-to-r from-violet-500 to-fuchsia-500 h-3 w-100 mt-1">
                 </div>
                 <div class="row">
-                    <div class="col">
-                        <h1 class="" >{{  }}</h1>
+                    <div class="col-12">
+                        <h1 class=" text-center font-semibold text-orange-500 fs-14 subpixel-antialiased mt-1" > {{  _instance_profilePagesStore.employeeDetails.client_details.client_name  }}</h1>
                     </div>
-                    <div class="col">{{  }}</div>
-                    <div class="col">{{  }}</div>
-                    <div class="col">{{  }}</div>
-
-
+                    <div class="col-12"> <h1 class=" fs-11 text-center font-semibold text-blue-900">{{ _instance_profilePagesStore.employeeDetails.client_details.address }}</h1></div>
+                    <div class="col-12"> <h1 class="fs-11 text-center font-semibold lining-nums ... text-blue-900">{{_instance_profilePagesStore.employeeDetails.client_details.authorised_person_contact_number  }}</h1></div>
+                    <div class="col-12"> <h1 class="fs-12 text-center font-semibold  text-blue-900">{{ _instance_profilePagesStore.employeeDetails.client_details.authorised_person_contact_email  }}</h1></div>
+                    <div class="col-12"> <h1 class="fs-12 text-center font-semibold  text-blue-900">{{ _instance_profilePagesStore.employeeDetails.email  }}</h1></div>
                 </div>
 
             </div>
@@ -578,6 +576,8 @@ onMounted(() => {
         reportManagerOption.value = res.data;
     });
     setvalue();
+
+    console.log();
 });
 
 const UploadEmpDocsPhoto = (e) => {
@@ -634,39 +634,26 @@ const saveEmpChangeInfoDetails = () => {
 
 }
 
-const blood_group_id = ref() ;
+const cmpBldGrp = computed(() => {
+    if (_instance_profilePagesStore.employeeDetails.get_employee_details.blood_group_id == 1) return "A Positive";
 
-switch (_instance_profilePagesStore.employeeDetails.get_employee_details.blood_group_id) {
-    case 8 :
-     blood_group_id.value = "O Negative" ;
-      break;
-     case 7 :
-     blood_group_id.value = "O Negative" ;
-      break;
-     case 6 :
-     blood_group_id.value = "O Negative" ;
-      break;
-     case 5 :
-     blood_group_id.value = "O Negative" ;
-      break;
-     case 4 :
-     blood_group_id.value = "O Negative" ;
-      break;
-     case 3 :
-     blood_group_id.value = "O Negative" ;
-      break;
-     case 2 :
-     blood_group_id.value = "O Negative" ;
-      break;
-     case 1 :
-     blood_group_id.value = "O Negative" ;
-      break;
-     case 1 :
-     blood_group_id.value = "O Negative" ;
-      break;
-     default:
-       blood_group_id.value = "-" ;
-}
+    else if (_instance_profilePagesStore.employeeDetails.get_employee_details.blood_group_id == 2) return "A Negative";
+    else if (_instance_profilePagesStore.employeeDetails.get_employee_details.blood_group_id == 3) return "B Positive";
+
+    else if (_instance_profilePagesStore.employeeDetails.get_employee_details.blood_group_id == 4) return "B Negative";
+
+
+    else if (_instance_profilePagesStore.employeeDetails.get_employee_details.blood_group_id == 5) return "AB Positive";
+
+    else if (_instance_profilePagesStore.employeeDetails.get_employee_details.blood_group_id == 6) return "AB Negative";
+
+    else if (_instance_profilePagesStore.employeeDetails.get_employee_details.blood_group_id == 7) return "O Positive";
+
+    else if (_instance_profilePagesStore.employeeDetails.get_employee_details.blood_group_id == 8) return "O Negative";
+
+})
+
+
 
 
 
