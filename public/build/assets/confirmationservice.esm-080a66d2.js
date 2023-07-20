0dac9b2846227319ad08e3551073007f9a1bfe33
Shelly <121927710+AntonyXavierShelton@users.noreply.github.com>
2023-07-20 20:15:45 +0530

    --- VJS BUILD ---

--- public/build/assets/confirmationservice.esm-080a66d2.js
+++
@@ -1,1 +0,0 @@
-import{a0 as m,ac as r}from"./toastservice.esm-b6cf114f.js";const e=Symbol();function c(){const o=m(e);if(!o)throw new Error("No PrimeVue Confirmation provided!");return o}var f={install:o=>{const i={require:n=>{r.emit("confirm",n)},close:()=>{r.emit("close")}};o.config.globalProperties.$confirm=i,o.provide(e,i)}};export{f as C,c as u};
