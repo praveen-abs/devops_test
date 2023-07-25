025399f87aa81e7b85c29c1ad7104cc71c1d82bd
Shelly <121927710+AntonyXavierShelton@users.noreply.github.com>
2023-07-25 12:45:26 +0530

    Dashboard Minor changes

--- resources/js/hrms/modules/dashboard/hr_dashboard/org_employee_details/org_employee_details.vue
+++ resources/js/hrms/modules/dashboard/hr_dashboard/org_employee_details/org_employee_details.vue
@@ -1,7 +1,6 @@
 <template>
-            <div class="col-sm-6 col-md-6 col-xl-3 col-lg-3 col-xxl-3">
           <div class="card">
-            <div class="px-1 card-body d-flex align-items-center">
+            <div class="px-3 card-body d-flex align-items-center">
               <div class="me-2">
                 <div class="icons bg-blue-lighten">
                   <i class="fas fa-users blue-darken"></i>
@@ -10,15 +9,13 @@
 
               <div class="">
                 <p class="mb-1 text-center fs-13 text-muted">Total Employees</p>
-                <p class="text-center text-primary fs-17">100</p>
+                <p class="text-center text-primary fs-17">0</p>
               </div>
             </div>
           </div>
-        </div>
 
-        <div class="col-sm-6 col-md-6 col-xl-3 col-lg-3 col-xxl-3">
           <div class="card">
-            <div class="px-1 card-body d-flex align-items-center">
+            <div class="px-2 card-body d-flex align-items-center">
               <div class="me-2">
                 <div class="icons bg-green-lighten">
                   <i class="fas fa-user-plus green-median"></i>
@@ -27,15 +24,13 @@
 
               <div class="">
                 <p class="mb-1 text-center fs-13 text-muted">New Employees</p>
-                <p class="text-center text-primary fs-17">100</p>
+                <p class="text-center text-primary fs-17">0</p>
               </div>
             </div>
           </div>
-        </div>
 
-        <div class="col-sm-6 col-md-6 col-xl-3 col-lg-3 col-xxl-3">
           <div class="card">
-            <div class="px-1 card-body d-flex align-items-center">
+            <div class="px-2 card-body d-flex align-items-center">
               <div class="me-2">
                 <div class="icons bg-pink-lighten">
                   <i class="fas fa-user-check pink-median"></i>
@@ -44,15 +39,13 @@
 
               <div class="">
                 <p class="mb-1 text-center fs-13 text-muted">Employees Present</p>
-                <p class="text-center text-primary fs-17">100</p>
+                <p class="text-center text-primary fs-17">0</p>
               </div>
             </div>
           </div>
-        </div>
 
-        <div class="col-sm-6 col-md-6 col-xl-3 col-lg-3 col-xxl-3">
           <div class="card">
-            <div class="px-1 card-body d-flex align-items-center">
+            <div class="px-2 card-body d-flex align-items-center">
               <div class="me-2">
                 <div class="icons bg-sky-lighten">
                   <i class="fas fa-user-minus sky-median"></i>
@@ -61,9 +54,8 @@
 
               <div class="">
                 <p class="mb-1 text-center fs-13 text-muted">Employees On Leave</p>
-                <p class="text-center text-primary fs-17">100</p>
+                <p class="text-center text-primary fs-17">0</p>
               </div>
             </div>
           </div>
-        </div>
 </template>
