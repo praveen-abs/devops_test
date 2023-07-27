f6ef4fc13b29fede8716046beb1b421da3ea349b
Pradeesh-ABS <140049235+Pradeesh-ABS@users.noreply.github.com>
2023-07-27 10:53:20 +0530

    WIP :: salary_advance

--- resources/js/hrms/modules/salary_loan_setting/salary_advance/salary_advance.vue
+++ resources/js/hrms/modules/salary_loan_setting/salary_advance/salary_advance.vue
@@ -52,23 +52,31 @@
                 <div class="row ml-1 mr-3 mt-2 ">
                     <div class="col-12 border-1 rounded-md h-28 d-flex flex-column align-items-center justify-content-between p-3 even-card shadow-sm mb-2 blink"
                         v-for="(item, index) in salaryStore.salaryAdvanceSettingsDetails" :key="index" :class="[]" >
-                        <div class="w-100 d-flex justify-content-between align-items-center">
-                            <h1 class="  fs-5">{{ item.settings.settings_name }}</h1>
-                            <button class=" underline text-blue-400 fs-5 " @click="viewDetails(item)">View Details</button>
-                        </div>
-                        <div class="w-100 d-flex justify-content-between align-items-center">
-                            <div>
-                                <h1 class=" fs-5" v-if="item.settings.deduction_period_of_months === 1">Deduct the amount in
-                                    the Upcomming Payroll</h1>
-                                <h1 class=" fs-5" v-else>The deduction can be made over a period of {{
-                                    item.settings.deduction_period_of_months }} months. </h1>
+                        <div class="row w-full">
+                            <div class="col">
+                                <h1 class="text-[15px]">{{ item.settings.settings_name }}</h1>
                             </div>
+                            <div class="col">
 
-                            <h1 class=" fs-5">Percentage of Salary Advance: {{ item.settings.percent_salary_adv }}%</h1>
-                            <h1 class=" fs-5">Employee Count : {{ item.settings.emp_count }}</h1>
-
+                            </div>
+                            <div class="col-4 ">
+                                <button class=" underline text-blue-400 fs-5 float-right" @click="viewDetails(item)">View Details</button>
+                            </div>
                         </div>
-
+                        <div class="row w-full">
+                            <div class="col">
+                                <h1 class="  text-[15px]" v-if="item.settings.deduction_period_of_months === 1">Deduct the amount in
+                                    the Upcomming Payroll</h1>
+                                    <h1 class=" text-[15px]" v-else>The deduction can be made over a period of {{
+                                        item.settings.deduction_period_of_months }} months. </h1>
+                            </div>
+                            <div class="col">
+                                <h1 class=" text-[15px]  text-right">Percentage of Salary Advance: {{ item.settings.percent_salary_adv }}%</h1>
+                            </div>
+                            <div class="col">
+                                <h1 class=" text-[15px] float-right">Employee Count : {{ item.settings.emp_count }}</h1>
+                            </div>
+                        </div>
                     </div>
 
                 </div>
