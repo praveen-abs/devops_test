631fba82a33e258f8059e97abcb9f496053fcc25
Pradeesh-ABS <140049235+Pradeesh-ABS@users.noreply.github.com>
2023-08-02 15:59:48 +0530

    minor change :: WelcomeMail

--- app/Mail/WelcomeMail.php
+++ app/Mail/WelcomeMail.php
@@ -45,13 +45,17 @@ class WelcomeMail extends Mailable
     {
 
 
-            $output = $this->view('vmt_welcomeemployee_email')
-            ->subject('ABShrms - Welcome')
-            ->with('uEmail', $this->uEmail)
-            ->with('uPassowrd', $this->uPassowrd)
-            ->with('loginLink', $this->loginLink)
-            ->with('image_view', $this->image_view)
-            ->with('client_code',$this->client_code);
+            // $output = $this->view('vmt_welcomeemployee_email')
+            // ->subject('ABShrms - Welcome')
+            // ->with('uEmail', $this->uEmail)
+            // ->with('uPassowrd', $this->uPassowrd)
+            // ->with('loginLink', $this->loginLink)
+            // ->with('image_view', $this->image_view)
+            // ->with('client_code',$this->client_code);
+
+            $output = $this->view('vmt_appointment_templates.mailtemplate_appointmentletter_ardensbusinesssolutionsprivatelimited')
+            ->subject('ABShrms - Welcome');
+            
 
 
         //Only for Employee Onboarding
