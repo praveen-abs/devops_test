d44371a5673e0dfd7d3f6da01723c1df95e0e1ef
Pradeesh-ABS <140049235+Pradeesh-ABS@users.noreply.github.com>
2023-08-02 14:14:48 +0530

    minor change ::

--- resources/js/hrms/modules/payroll/payroll_setting/payroll_setup/salary_components/earings/earings.vue
+++ resources/js/hrms/modules/payroll/payroll_setting/payroll_setup/salary_components/earings/earings.vue
@@ -2,7 +2,8 @@
     <div class="w-full ">
         <section id="header" class="flex justify-between w-full my-4 ">
             <p class=" mx-1 font-semibold fs-5.5">Salary Compenents</p>
-            <div><button class="btn btn-orange" @click="usePayroll.dailogNewSalaryComponents = true">Add Components</button>
+            <div><button class="btn btn-orange w-[160px]" @click="usePayroll.dailogNewSalaryComponents = true">
+                <i class="pi pi-plus"></i> Add Components</button>
             </div>
         </section>
         <div class="table-responsive">
