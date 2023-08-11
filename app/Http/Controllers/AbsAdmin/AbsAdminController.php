a26d51e05e8098044d1e0927c724ad7373e8ea3e
Praveen ABS <140048006+praveen-abs@users.noreply.github.com>
2023-08-11 19:54:27 +0530

    WIP : AbsAdmin page for handling core features.

---
+++ app/Http/Controllers/AbsAdmin/AbsAdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\AbsAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AbsAdminController extends Controller
+{
+    public function showAbsAdminPage(Request $request){
+
+    }
+
+    public function runMigrations(Request $request){
+
+    }
+}
