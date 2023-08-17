8e8add243c7669de86b66b0939cf8eb3380f2a4a
Shelly <121927710+AntonyXavierShelton@users.noreply.github.com>
2023-08-17 15:08:12 +0530

    --- VJS BUILD ---

--- resources/js/hrms/modules/Organization/router/router.js
+++ resources/js/hrms/modules/Organization/router/router.js
@@ -5,7 +5,7 @@ import BulkOnboarding from '../BulkOnboarding/BulkOnboarding.vue'
 
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    // history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: "/quickEmployeeOnboarding/:module",
