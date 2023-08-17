a7bc65e49797a3729a5578d6e379a0117454c1e3
PRADEESH <140049235+Pradeesh-ABS@users.noreply.github.com>
2023-08-17 15:51:20 +0530

    minor change

--- resources/js/hrms/modules/approvals/salary_advance_loan/approvals_salary_advance.js
+++ resources/js/hrms/modules/approvals/salary_advance_loan/approvals_salary_advance.js
@@ -1,5 +1,4 @@
 import "primevue/resources/themes/lara-light-blue/theme.css";
-import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import '../../../assests/tailwind.css';
 
