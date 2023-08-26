faf05afd7554f83420a664dd6d3622ef5e2052e9
deeban-abs <deepan@abshrms.com>
2023-08-26 12:09:23 +0530

    Bug Fix : Changed implicit variable to prevent composer loggin error on login page.

--- app/Services/VmtEmployeeService.php
+++ app/Services/VmtEmployeeService.php
@@ -197,7 +197,7 @@ class VmtEmployeeService
             ]);
         }
     }
-    private function createOrUpdate_User($data, $can_onboard_employee, $user_id = null, $onboard_type)
+    private function createOrUpdate_User($user_id = null,$data, $can_onboard_employee,  $onboard_type)
     {
         $newUser = null;
 
