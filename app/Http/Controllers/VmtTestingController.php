8fa60cd7c5822b5d8007a9c071527567da4877c2
PRADEESH <140049235+Pradeesh-ABS@users.noreply.github.com>
2023-08-26 19:05:39 +0530

    WIP :: payslip v2

--- app/Http/Controllers/VmtTestingController.php
+++ app/Http/Controllers/VmtTestingController.php
@@ -769,8 +769,8 @@ class VmtTestingController extends Controller
         $appoinmentPath = "";
         $client_name = strtolower(str_replace(' ', '_', sessionGetSelectedClientName()));
 //dd($client_name);
-        $html = view('appointment_mail_templates.appointment_letter_langro_india_pvt_ltd',$data);
-return  $html;
+        $html = view('dynamic_payslip_templates.dynamic_payslip_v2',$data);
+    //    return  $html;
                         $options = new Options();
                         $options->set('isHtml5ParserEnabled', true);
                         $options->set('isRemoteEnabled', true);
@@ -798,9 +798,9 @@ return  $html;
      public function Tesingtdsworksheet(Request $request){
 
 
-            $html = view('investmentTdsWorkSheet.TDS_work_sheet');
+            $html = view('dynamic_payslip_templates.dynamic_payslip_v2');
 
-            return $html;
+            // return $html;
 
             $options = new Options();
             $options->set('isHtml5ParserEnabled', true);
---
+++ resources/views/dynamic_payslip_templates/dynamic_payslip_v2.blade.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en" xmlns:v="urn:schemas-microsoft-com:vml">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no, date=no, address=no, email=no, url=no">
+    <meta name="color-scheme" content="light dark">
+    <meta name="supported-color-schemes" content="light dark">
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Lobster&family=Poppins&display=swap');
+
+        * {
+            font-family: 'Lobster', cursive;
+            font-family: 'Poppins', sans-serif !important;
+        }
+
+        .border {
+            border: 1px solid #f3f4f6 !important;
+        }
+
+        .float-right {
+            float: right
+        }
+
+        .float-left {
+            float: left
+        }
+
+        .td {
+            height: 10px !important;
+        }
+
+        .marign-top1 {}
+
+        p {
+            color: #000;
+        }
+    </style>
+</head>
+
+
+<body>
+    <table style="background-color: white; width:100%;">
+        <tr>
+            <td colspan="3">
+                <h1 style="color:black; font-size:16px">ARDENS BUSINESS SOLUTIONS PRIVATE LIMITED</h1>
+                <p style="color:black; font-size:10px; margin-top:-8px;">Lorem ipsum dolor sit, amet consectetur
+                    adipisicing elit. Fuga, voluptates.</p>
+            </td>
+            <td colspan="1" style=" ">
+                <img src="" alt="Image Not Found" style=" color:black;  float: right; margin-top:10px">
+            </td>
+        </tr>
+        <tr>
+            <td style="border-top:#f9be00 1px solid; width:100% " colspan="4"></td>
+        </tr>
+        <tr>
+            <td colspan="4"style="padding: 0 0 10px 0;">
+                <h1 style="color:black; font-size:14px;">Payslip for the month of April 2023</h1>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="1">
+                <h1
+                    style="color:#f9be00; font-size:14px; letter-spacing: 0.4px; margin-top:-18px; padding-bottom:12px; width:200px; ">
+                    EMPLOYEE PAY SUMMARY</h1>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">Employee Name : </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">Designation : </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">Date Joining : </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">Pay Period : </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">Pay Date : </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">Bank Account No: </p>
+            </td>
+            <td colspan="2">
+                <p style="color:gray; font-size:12px; margin-top:-14px;">&nbsp;</p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">Pardeesh </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">Manager Operations </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">01/06/2023 </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">April 2023 </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">10/05/2023 </p>
+                <p style="color:gray; font-size:12px; margin-top:-14px;">2023390109 </p>
+
+
+            </td>
+            <td colspan="1">
+                <p style=" font-size:14px; margin-top:-14px; text-align:center;">Employee Net Pay </p>
+                <h1 style=" font-size:32px; margin-top:-12px;color:#000;text-align:center; ">82,54,82,500.00</h1>
+                <p style=" font-size:12px; margin-top:-18px; text-align:center; color:gray; ">Paid Days : 30 | LOP Day :
+                    0 | ARREAR DAYS : </p>
+                <p style=" font-size:12px; margin-top:-12px; text-align:center; color:#fff"></p>
+            </td>
+        </tr>
+        <tr>
+            <td style="border-top:#f9be00 1px solid; width:100% ; padding-bottom:10px;" colspan="4"></td>
+        </tr>
+
+        <tr>
+            <td colspan="4">
+                <table style="width:100%;">
+                    <tr style="padding:14px 0;">
+                        <td colspan="2">
+                            <h1 style="color:#f9be00; font-size:12px;  margin:-6px 0 2px 0 ;">EARNINGS
+                            </h1>
+                        </td>
+                        <td colspan="1">
+                            <h1 style="color:#f9be00; font-size:12px ; margin:-6px 0 2px 0;text-align:right;">AMOUNT
+                            </h1>
+                        </td>
+                        <td colspan="1">
+                            <h1 style="color:#f9be00; font-size:12px ; margin:-6px 0 2px 0;text-align:right;">ARREARS
+                            </h1>
+                        </td>
+                        <td colspan="1">
+                            <h1 style="color:#f9be00; font-size:12px ;  margin:-6px 0 2px 0;text-align:right;">YEAR TO
+                                DATE</h1>
+                        </td>
+                    </tr>
+                    {{-- <tr>
+                        <td style="border-bottom:1px dashed gray;margin-bottom:20px;" colspan="5"></td>
+                    </tr> --}}
+                    <tr style=" ">
+                        <td colspan="2" style="margin-bottom: auto !important;">
+                            <p style=" font-size:12px; text-algin:left;margin-top:-7px;">Basic </p>
+                            <p style=" font-size:12px ;text-algin:left; margin-top:-7px;">House Education Allowance</p>
+                            <p style=" font-size:12px;text-algin:left;margin-top:-7px; ">Children Education Allowance
+                            </p>
+                            <p style=" font-size:12px;text-algin:left;margin-top:-7px;">Fixed Allowance</p>
+                            <p style=" font-size:12px;text-algin:left;margin-top:-7px; ">Total</p>
+                        </td>
+                        <td colspan="1" style="margin-bottom: auto !important;">
+                            <p style=" font-size:12px;margin-top:-7px; text-align:right; ">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px;margin-top:-7px;text-align:right; ">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">&nbsp;</p>
+                        </td>
+                        <td colspan="1" style="margin-bottom: auto !important;">
+                            <p style=" font-size:12px;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px;margin-top:-7px;text-align:right; ">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">&nbsp;</p>
+                        </td>
+                        <td colspan="1">
+                            <p style=" font-size:12px;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px;margin-top:-7px;text-align:right; ">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">&nbsp;</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="border-top:#f9be00 1px solid; width:100%;margin-top:0px ; " colspan="5"></td>
+                    </tr>
+
+                    <tr style="padding-bottom:12px;">
+                        <td colspan="2">
+                            <h1 style="color:#f9be00; font-size:12px; text-align:left; margin:-4px 0 0px 0 ">DEDUTIONS
+                            </h1>
+                        </td>
+                        <td colspan="1">
+                            <h1
+                                style="color:#f9be00; font-size:12px ;text-align:left; margin:-4px 0 0px 0;text-align:right;">
+                                AMOUNT</p>
+                        </td>
+                        <td colspan="1">
+                            <h1
+                                style="color:#f9be00; font-size:12px ;text-align:left; margin:-4px 0 0px 0;text-align:right;">
+                                ARREARS</h1>
+                        </td>
+                        <td colspan="1">
+                            <h1
+                                style="color:#f9be00; font-size:12px ; text-align:left; margin:-4px 0 0 0;text-align:right;">
+                                YEAR TO DATE</h1>
+                        </td>
+                    </tr>
+                    {{-- <tr>
+                        <td style="border-bottom:1px dashed gray;margin-bottom:20px;" colspan="5"></td>
+                    </tr> --}}
+                    <tr style=" ">
+                        <td colspan="2" style="margin-bottom: auto !important;">
+                            <p style=" font-size:12px; text-algin:left;margin-top:-7px;">Basic </p>
+                            <p style=" font-size:12px ;text-algin:left; margin-top:-7px;">House Education Allowance</p>
+                            <p style=" font-size:12px;text-algin:left;margin-top:-7px; ">Children Education Allowance
+                            </p>
+                            <p style=" font-size:12px;text-algin:left;margin-top:-7px;">Fixed Allowance</p>
+                            <p style=" font-size:12px;text-algin:left;margin-top:-7px; ">Total</p>
+                        </td>
+                        <td colspan="1" style="margin-bottom: auto !important;">
+                            <p style=" font-size:12px;margin-top:-7px;text-align:right; ">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">&nbsp;</p>
+                        </td>
+                        <td colspan="1" style="margin-bottom: auto !important;">
+                            <p style=" font-size:12px;margin-top:-7px;  text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">&nbsp;</p>
+                        </td>
+                        <td colspan="1">
+                            <p style=" font-size:12px;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">sajd</p>
+                            <p style=" font-size:12px ;margin-top:-7px; text-align:right;">&nbsp;</p>
+                        </td>
+                    </tr>
+                    <tr style=" width:100%; ">
+                        <td colspan="5" style=" width:100%; height:40px !important;padding:0; ">
+                            <p
+                                style="border-radius:4px; background-color: #fff7db; font-size:12px; width:100%; padding:8px;">
+                                NET PAY (Gross Earnings - Total Deduction) <span style="margin-left: 18%; font-size:12px;">
+                                    400250</span></p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="5">
+                            <p style="font-size: 10px; margin-top:-12px;">Total Net Payable 1,05,578.00 (indian rupee
+                                One Lakh Five Thousand Five Hundred Seventy-Eight Only)</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="4">
+                <h1 style="color:#f9be00; margin-bottom:-8px; margin-top:-6px; font-size:14px;">LEAVE DETAILS</p>
+            </td>
+        </tr>
+
+        <tr class="td" style="height: 20px;">
+            <td colspan="4">
+                <table style="width: 100%;" >
+                    <tr>
+                        <td class="" style="height: 20px; width:25%;" >
+                            <p style="font-size:10px;color:gray; ">Leave Type</p>
+                            <p style="font-size:11px; margin-top:-8px">6</p>
+                        </td>
+                        <td colspan="1" style=" width:25%;">
+                            <p style="font-size:10px;color:gray; ">Opening Balance</p>
+                            <p style="font-size:11px; margin-top:-8px">8</p>
+                        </td>
+                        <td colspan="1" style=" width:25%;">
+                            <p style="font-size:10px;color:gray;">Avalied</p>
+                            <p style="font-size:11px; margin-top:-8px">2</p>
+                        </td>
+                        <td colspan="1" style=" width:25%;">
+                            <p style="font-size:10px;color:gray;">Closing Balance</p>
+                            <p style="font-size:11px; margin-top:-8px">2</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <tr>
+            <td style="border-top:#f9be00 1px solid; width:100%;margin-top:0px ; " colspan="4"></td>
+        </tr>
+        <tr>
+            <td colspan="4">
+                <p style="font-size: 10px; text-align:center;">- This is a system generated payslip, hence the signature is not required.-</p>
+            </td>
+        </tr>
+        
+        <tr>
+            <td colspan="4">
+               <img src="" alt="image Not Found" style="border:2px solid black; float: right;" >
+               <p style="float:right; ">abs_logo</p>
+               <p style="float:right; color:gray; font-size:12px;">Powered By</p>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
