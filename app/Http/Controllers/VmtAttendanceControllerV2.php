2873dbb4d7d45bb2a23facfa3a0ef6480714331f
Karthi282k <karthigaiselvan28062000@gmail.com>
2023-08-30 15:54:22 +0530

    Wrote Base code for Attendance Service V2

---
+++ app/Http/Controllers/VmtAttendanceControllerV2.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\VmtEmployeeAttendanceV2;
+
+class VmtAttendanceControllerV2 extends Controller
+{
+    //
+}
---
+++ app/Models/VmtEmployeeAttendanceV2.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VmtEmployeeAttendanceV2 extends Model
+{
+    use HasFactory;
+    protected $table = 'vmt_employee_attendance_v2';
+}
