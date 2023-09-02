3189178c67c07b527a8710b0a4b412b980b81f3c
Narasimman <narasimman@abshrms.com>
2023-09-02 16:23:11 +0530

    --- VJS BUILD ---

---
+++ public/build/assets/confirmationservice.esm-ec960ba4.js
@@ -0,0 +1,1 @@
+import{a5 as m,aq as r}from"./toastservice.esm-92ad56cd.js";const e=Symbol();function f(){const o=m(e);if(!o)throw new Error("No PrimeVue Confirmation provided!");return o}var s={install:o=>{const i={require:n=>{r.emit("confirm",n)},close:()=>{r.emit("close")}};o.config.globalProperties.$confirm=i,o.provide(e,i)}};export{s as C,f as u};
