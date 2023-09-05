609f419e8172edc7d3c84fce8fd5ca709dd65f13
Vishnu-ABS <vishnu@abshrms.com>
2023-09-05 13:50:15 +0530

    Minor Change :: tittle changes to title

--- database/seeders/Seeder_VmtAppModuleSeeder.php
+++ database/seeders/Seeder_VmtAppModuleSeeder.php
@@ -24,31 +24,31 @@ class Seeder_VmtAppModuleSeeder extends Seeder
         DB::table('vmt_app_modules')->truncate();
 
         DB::table('vmt_app_modules')->insert([
-            ['id'=>'1','module_name'=>'MOBILE_APP_SETTINGS',"tittle"=>'mobile_app_settings'],
-            ['id'=>'2','module_name'=>'MAIN_DASHBOARD',"tittle"=>'dashboard'],
-            ['id'=>'3','module_name'=>'ATTENDANCE_TIMESHEET',"tittle"=>'attendance_timesheet'],
-            ['id'=>'4','module_name'=>'LEAVE',"tittle"=>'leave'],
-            ['id'=>'5','module_name'=>'PMS',"tittle"=>'pms'],
-            ['id'=>'6','module_name'=>'PAYROLL',"tittle"=>'payroll'],
+            ['id'=>'1','module_name'=>'MOBILE_APP_SETTINGS',"title"=>'mobile_app_settings'],
+            ['id'=>'2','module_name'=>'MAIN_DASHBOARD',"title"=>'dashboard'],
+            ['id'=>'3','module_name'=>'ATTENDANCE_TIMESHEET',"title"=>'attendance_timesheet'],
+            ['id'=>'4','module_name'=>'LEAVE',"title"=>'leave'],
+            ['id'=>'5','module_name'=>'PMS',"title"=>'pms'],
+            ['id'=>'6','module_name'=>'PAYROLL',"title"=>'payroll'],
         ]);
         DB::table('vmt_app_sub_modules')->truncate();
 
         DB::table('vmt_app_sub_modules')->insert([
-            ['id'=>'1','sub_module_name'=>'IS_MOBILEAPP_ENABLED',"tittle"=>'mobile_app'],
-            ['id'=>'2','sub_module_name'=>'CAN_CHECKIN',"tittle"=>'check_in'],
-            ['id'=>'3','sub_module_name'=>'CAN_CHECKOUT',"tittle"=>'check-out'],
-            ['id'=>'4','sub_module_name'=>'CHECKIN_LOCATION',"tittle"=>'location_capture'],
-            ['id'=>'5','sub_module_name'=>'CHECKIN_SELFIE',"tittle"=>'check_in_selfie'],
-            ['id'=>'6','sub_module_name'=>'CHECKOUT_SELFIE',"tittle"=>'check_out_selfie'],
-            ['id'=>'7','sub_module_name'=>'REIMBURSEMENT_ON_CHECKOUT',"tittle"=>'reimbursement_while_check_out'],
-            ['id'=>'8','sub_module_name'=>'Absent Regularization',"tittle"=>'absent_regularization'],
-            ['id'=>'9','sub_module_name'=>'Attendance Regularization',"tittle"=>'attendance_regularization'],
-            ['id'=>'10','sub_module_name'=>'CAN_APPLY_LEAVE',"tittle"=>'leave_apply'],
-            ['id'=>'11','sub_module_name'=>'Salary Advance and Loan',"tittle"=>'salary_advance_and_loan'],
-            ['id'=>'12','sub_module_name'=>'Investments',"tittle"=>'investments'],
-            ['id'=>'13','sub_module_name'=>'PMS',"tittle"=>'pms'],
-            ['id'=>'14','sub_module_name'=>'Exit Apply',"tittle"=>'exit_apply'],
-            ['id'=>'15','sub_module_name'=>'IS_ENABLED',"tittle"=>'is_enabled'],
+            ['id'=>'1','sub_module_name'=>'IS_MOBILEAPP_ENABLED',"title"=>'mobile_app'],
+            ['id'=>'2','sub_module_name'=>'CAN_CHECKIN',"title"=>'check_in'],
+            ['id'=>'3','sub_module_name'=>'CAN_CHECKOUT',"title"=>'check-out'],
+            ['id'=>'4','sub_module_name'=>'CHECKIN_LOCATION',"title"=>'location_capture'],
+            ['id'=>'5','sub_module_name'=>'CHECKIN_SELFIE',"title"=>'check_in_selfie'],
+            ['id'=>'6','sub_module_name'=>'CHECKOUT_SELFIE',"title"=>'check_out_selfie'],
+            ['id'=>'7','sub_module_name'=>'REIMBURSEMENT_ON_CHECKOUT',"title"=>'reimbursement_while_check_out'],
+            ['id'=>'8','sub_module_name'=>'Absent Regularization',"title"=>'absent_regularization'],
+            ['id'=>'9','sub_module_name'=>'Attendance Regularization',"title"=>'attendance_regularization'],
+            ['id'=>'10','sub_module_name'=>'CAN_APPLY_LEAVE',"title"=>'leave_apply'],
+            ['id'=>'11','sub_module_name'=>'Salary Advance and Loan',"title"=>'salary_advance_and_loan'],
+            ['id'=>'12','sub_module_name'=>'Investments',"title"=>'investments'],
+            ['id'=>'13','sub_module_name'=>'PMS',"title"=>'pms'],
+            ['id'=>'14','sub_module_name'=>'Exit Apply',"title"=>'exit_apply'],
+            ['id'=>'15','sub_module_name'=>'IS_ENABLED',"title"=>'is_enabled'],
         ]);
 
         DB::table('vmt_app_sub_modules_links')->truncate();
