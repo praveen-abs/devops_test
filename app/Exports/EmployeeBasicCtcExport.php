3a5b4dd15a2513ab00afe6a89eff6f62f106c108
deeban-abs <139613302+deebanraj232k@users.noreply.github.com>
2023-09-07 10:27:47 +0530

    DB:migrate(add new column)(table::vmt_employee_compensatory_details)

--- app/Exports/EmployeeBasicCtcExport.php
+++ app/Exports/EmployeeBasicCtcExport.php
@@ -3,21 +3,27 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
-
-class EmployeeBasicCtcExport implements FromCollection
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+class EmployeeBasicCtcExport implements FromArray,ShouldAutoSize
 {
 
     protected $data;
-     
-    public function __construct($data)
+    protected $headers;
+    
+    public function __construct($data,$headers)
     {
         $this->data = $data;
+        $this->headers = $headers;
     }
-    /**
-     * @return \Illuminate\Support\Collection
-     */
-    public function collection()
+
+    public function headings(): array
     {
-        return  $this->data;
+        return [$this->headers];
     }
+    public function array(): array
+    {
+        return [ $this->data];
+    }
+ 
 }
