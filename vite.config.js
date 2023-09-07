c541c08be926e87f6621f917915467e3dbcfa195
Shelly <121927710+AntonyXavierShelton@users.noreply.github.com>
2023-09-07 13:18:18 +0530

    --- VJS BUILD ---

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+
     plugins: [
         { src: '@/plugins/vue3-html2pdf', mode: 'client' },
         vue(),
@@ -189,8 +190,7 @@ export default defineConfig({
 
 
             ],
-
-            refresh: true,
+            refresh: ['resources/views/**'],
         }),
     ],
     resolve: {
