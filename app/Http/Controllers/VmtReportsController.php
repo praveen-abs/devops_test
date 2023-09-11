9476c888621ea71f562f4dad6275d3d4697f30c3
Karthi282k <karthigaiselvan28062000@gmail.com>
2023-09-11 20:08:46 +0530

    CTC Report :: Minor changes

--- app/Http/Controllers/VmtReportsController.php
+++ app/Http/Controllers/VmtReportsController.php
@@ -628,7 +628,7 @@ class VmtReportsController extends Controller
     {
 
 
-        return  $reportsService->getEmployeesCTCDetails($request->type , $request->client_id, $request->active_status, $request->department_id,$request->date);
+        return  $reportsService->getEmployeesCTCDetails($request->type , $request->legal_entity, $request->active_status, $request->department_id,$request->date);
     }
 
     public function generateEmployeesCTCReportData(Request $request, VmtReportsservice $reportsService)
@@ -636,7 +636,7 @@ class VmtReportsController extends Controller
         $date = Carbon::now();
         $client_id = array(1);
         $Category = 'All';
-        $emp_ctc_data = $reportsService->getEmployeesCTCDetails($request->type , $request->client_id, $request->active_status, $request->department_id,$request->date);
+        $emp_ctc_data = $reportsService->getEmployeesCTCDetails($request->type , $request->legal_entity, $request->active_status, $request->department_id,$request->date);
         $headers = array();
         foreach ($emp_ctc_data as $key => $value) {
             $headings = $key;
