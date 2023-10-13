314f6bc2cdac90b536399ea94a139e25734cb0bd
Vishnu-ABS <vishnu@abshrms.com>
2023-10-13 13:08:05 +0530

    cleared build files

--- public/build/assets/api-71d91cc8.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./index-362795f3.js";import{r as t,o as s}from"./app-68129d7d.js";const f={__name:"api",setup(c){const r=t([]),o=t(!0),e=async()=>{try{const a=await n.get("http://115.240.254.90:81/iclock/Main.aspx");r.value=a.data}catch(a){console.error("Error fetching data:",a)}finally{o.value=!1}};return s(()=>{e()}),(a,i)=>null}};export{f as default};
