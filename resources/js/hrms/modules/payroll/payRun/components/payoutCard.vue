64abc2367a75d99e48b8538992586e3b16849315
Shelly <antony@abshrms.com>
2023-10-20 12:20:02 +0530

    Minor changes in employee payout card

--- resources/js/hrms/modules/payroll/payRun/components/payoutCard.vue
+++ resources/js/hrms/modules/payroll/payRun/components/payoutCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="grid grid-cols-6 gap-4">
         <div v-for="item in source">
-            <p class="font-medium text-gray-500 text-lg"  >
+            <p class="font-medium text-gray-500 text-lg">
                 {{ convertToTitleCase(item.title) }}
             </p>
             <p class="font-semibold text-xl">
@@ -20,11 +20,16 @@ const props = defineProps({
     },
 });
 
-function convertToTitleCase(str) {
-    if(str){
-        const title =  str.replace(/\b\w/g, match => match.toUpperCase());
-       const converted = title.replace(/_/g, ' ');
-        return converted
+function convertToTitleCase(originalString) {
+    if (originalString) {
+        //     const title =  str.replace(/\b\w/g, match => match.toUpperCase());
+        //    const converted = title.replace(/_/g, ' ');
+        //     return converted
+        const convertedString = originalString
+            .split('_')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(' ');
+            return convertedString
     }
 }
 
